0fa41f4f82751aded09e9d2497551db3a5a1aff6
Daniel <daniel10gja@gmail.com>
2024-01-11 22:03:00 -0300

    aula 02

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-07/nomemclaturaProjetosJava/src/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main(String[] args) {
+
+         /*➔Classes, Interfaces e Enum: A primeira letra deve ser maiúsculas, caso o nome seja formado por mais de uma palavra,
+         as demais palavras devem ter sua primeira letra maiúscula.*/
+        System.out.println("NomeDeUmaClasse");
+
+         /*➔Métodos, Atributos e Variáveis: A primeira letra do método deve ser minúscula e após devemos aplicar o camelCase.*/
+        System.out.println("variavelQualquer");
+
+         /*➔Constantes: Todas as letras devem ser maiúsculas.*/
+        System.out.println("NOME_DE_UMA_CONSTANTE");
+
+    }
+
+}
