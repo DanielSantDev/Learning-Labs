e6548f49bae94073f3ababc7a955a25488817bc4
Daniel <daniel10gja@gmail.com>
2024-01-17 01:15:55 -0300

    Create 01-variaveis-operadores.js

---
+++ desenvolvimento-frontend-angular/02 - Fundamentos de Front-end/01-variaveis-operadores.js
@@ -0,0 +1,16 @@
+let variavel = 10;
+
+const pi = 3.14;
+
+console.log(variavel)
+console.log(pi)
+
+/*Exercício Conta Simples
+-----------------------*/
+
+let a = 10, b = 40;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
