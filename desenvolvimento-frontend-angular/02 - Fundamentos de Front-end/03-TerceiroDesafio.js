618b6da5e16d7ba019a4b34c71bcc1d90ff51fc2
Daniel <daniel10gja@gmail.com>
2024-01-19 00:03:46 -0300

    Create 03-TerceiroDesafio.js

---
+++ desenvolvimento-frontend-angular/02 - Fundamentos de Front-end/03-TerceiroDesafio.js
@@ -0,0 +1,14 @@
+let nota1 = 9.5;
+let nota2 = 7.5;
+let nota3 = 10;
+let media;
+
+media = ((nota1 + nota2 + nota3) / 3);
+
+if ( media >= 7) {
+    console.log('Parabens ! Passou no exame !');
+} else if( media < 7 ) {
+    console.log('Reprovado no semestre !');
+} else {
+    console.log('ERRO!');
+}
