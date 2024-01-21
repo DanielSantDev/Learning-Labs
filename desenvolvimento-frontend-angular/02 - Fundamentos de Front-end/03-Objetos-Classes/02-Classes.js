eb3cec13d8883668c28e942c3cedcfe144a1a62e
Daniel <daniel10gja@gmail.com>
2024-01-21 02:30:25 -0300

    Update 02-Classes.js

--- desenvolvimento-frontend-angular/02 - Fundamentos de Front-end/03-Objetos-Classes/02-Classes.js
+++ desenvolvimento-frontend-angular/02 - Fundamentos de Front-end/03-Objetos-Classes/02-Classes.js
@@ -1,26 +1,30 @@
 // Definindo a classe Carro
 class Carro {
-    // Construtor da classe
+    // Construtor da classe(ou seja, oq acontece quando uma classe é instanciada ?)
     constructor(modelo, cor, anoFabricacao) {
-      this.modelo = modelo;
-      this.cor = cor;
-      this.anoFabricacao = anoFabricacao;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.anoFabricacao = anoFabricacao;
     }
-  
+
     // Método para exibir informações do carro
     exibirInfo() {
-      console.log(`Modelo: ${this.modelo}`);
-      console.log(`Cor: ${this.cor}`);
-      console.log(`Ano de Fabricação: ${this.anoFabricacao}`);
+        console.log(`Modelo: ${this.modelo}`);
+        console.log(`Cor: ${this.cor}`);
+        console.log(`Ano de Fabricação: ${this.anoFabricacao}`);
     }
-  }
-  
-  // Criando instância da classe Carro
-  let meuCarro = new Carro("Civic", "Prata", 2020);
-  
-  // Acessando propriedades da instância
-  console.log(`Modelo do carro: ${meuCarro.modelo}`);
-  console.log(`Cor do carro: ${meuCarro.cor}`);
-  
-  // Chamando o método para exibir informações do carro
-  meuCarro.exibirInfo();
+}
+
+// Criando instância da classe Carro
+let meuCarro1 = new Carro("Civic", "Prata", 2020);
+let meuCarro2 = new Carro("Fiat", "Uno", 2018)
+
+// Acessando propriedades da instância
+console.log(`Modelo do carro: ${meuCarro1.modelo}`);
+console.log(`Cor do carro: ${meuCarro1.cor}`);
+
+console.log(`Modelo do carro: ${meuCarro2.modelo}`);
+console.log(`Cor do carro: ${meuCarro2.cor}`);
+
+// Chamando o método para exibir informações do carro
+meuCarro.exibirInfo();
