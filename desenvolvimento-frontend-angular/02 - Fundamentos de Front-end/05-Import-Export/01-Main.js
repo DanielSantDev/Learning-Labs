d0ecfac7f819d70a2ca4db2df34d40e5afb1bb73
Daniel <daniel10gja@gmail.com>
2024-01-21 17:36:22 -0300

    Create 01-Main.js

---
+++ desenvolvimento-frontend-angular/02 - Fundamentos de Front-end/05-Import-Export/01-Main.js
@@ -0,0 +1,14 @@
+//Importando, Destructuring
+const { gets, print } = require('./01-FuncoesAuxiliares');
+
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
