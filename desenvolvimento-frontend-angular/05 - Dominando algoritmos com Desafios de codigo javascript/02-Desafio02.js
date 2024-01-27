a96771d2db4fa5a788535423f4992524b2bcd4e2
Daniel <daniel10gja@gmail.com>
2024-01-26 23:32:48 -0300

    Update 02-Desafio02.js

--- desenvolvimento-frontend-angular/05 - Dominando algoritmos com Desafios de codigo javascript/02-Desafio02.js
+++ desenvolvimento-frontend-angular/05 - Dominando algoritmos com Desafios de codigo javascript/02-Desafio02.js
@@ -25,3 +25,25 @@ As conas bancárias são diferentes
 **
 */
 
+//objetos JSON 1 e 2
+const cliente1 = {
+    nome: gets(),
+    conta: gets(),
+    saldo: parseFloat(gets())
+};
+
+const cliente2 = {
+    nome: gets(),
+    conta: gets(),
+    saldo: parseFloat(gets())
+};
+
+function comparaConta() {
+    if ((cliente1.nome === cliente2.nome) && (cliente1.conta === cliente2.conta) && (cliente1.saldo === cliente2.saldo)) {
+        print("As contas bancárias são iguais.");
+    } else {
+        print("As contas bancárias são diferentes.")
+    }
+}
+
+comparaConta();
