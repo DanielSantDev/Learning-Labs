7c9d08692fd25538e394b074ea72fed3527e8843
Daniel <daniel10gja@gmail.com>
2024-01-26 23:28:00 -0300

    Create 02-Desafio02.js

---
+++ desenvolvimento-frontend-angular/05 - Dominando algoritmos com Desafios de codigo javascript/02-Desafio02.js
@@ -0,0 +1,27 @@
+/*Desafio
+O objetivo deste desafio é criar um programa em JavaScript que compare duas contas bancárias e determine se elas possuem ou não as mesmas informações. O programa deve criar dois objetos JSON contendo o número da conta e o saldo da conta.
+
+Entrada
+O programa receberá os dados necessários para criar duas contas bancárias para comparação:
+
+Conta Bancária 1:
+
+Número da Conta (número inteiro): número da conta da primeira conta bancária.
+Saldo (número): saldo da primeira conta bancária.
+Conta Bancária 2:
+
+Número da Conta (número inteiro): número da conta da segunda conta bancária.
+Saldo (número): saldo da segunda conta bancária.
+Importante: a entrada deverá ocorrer de acordo com a ordem de informações fornecidas acima.
+
+
+Saída
+Se todos os dados das contas bancárias forem estritamente iguais, o programa deverá retornar a mensagem:
+As contas bancárias são iguais.
+
+Caso contrário, o programa deverá retornar a mensagem:
+As conas bancárias são diferentes
+***
+**
+*/
+
