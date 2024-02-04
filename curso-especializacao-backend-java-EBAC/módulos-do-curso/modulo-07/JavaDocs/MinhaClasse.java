db63aec38318eab653916980421a4f381fdbf95c
Daniel <daniel10gja@gmail.com>
2024-02-04 11:43:46 -0300

    Create MinhaClasse.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-07/JavaDocs/MinhaClasse.java
@@ -0,0 +1,27 @@
+/**
+ * Esta é a descrição da classe.
+ */
+public class MinhaClasse {
+    /**
+     * Descrição do método.
+     * @param parametro Descrição do parâmetro.
+     * @return Descrição do valor de retorno.
+     */
+    public int meuMetodo(int parametro) {
+        // implementação
+        return parametro;
+    }
+
+    /**
+     * Calcula a soma de dois números.
+     * @param a O primeiro número.
+     * @param b O segundo número.
+     * @return A soma dos dois números.
+     * @throws ArithmeticException Se a soma estourar o limite do tipo de dado.
+     * @see OutraClasse#outroMetodo
+     */
+    public int somar(int a, int b) throws ArithmeticException {
+        // implementação
+        return  a + b;
+    }
+}
