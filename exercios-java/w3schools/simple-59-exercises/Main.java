e1c65fc7229086d8b9e427def8fdf46c9f7b7890
Daniel <daniel10gja@gmail.com>
2024-02-05 09:23:12 -0300

    exercícios iniciais

---
+++ exercios-java/w3schools/simple-59-exercises/Main.java
@@ -0,0 +1,99 @@
+//Simple Java exercises
+//access this list in: w3schools.com/java/exercise.asp
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Java Sytax 1,2
+//        System.out.println("Hello World");
+
+        //This is a single-line comment
+        /*This is a multi-line comment*/
+//__________________________________________________________________
+        //Java Variables 3,4,5,6,7
+//        String carName = "Volvo";
+
+//        int maxSpeed = 120;
+
+//        int x = 5;
+//        int y = 10;
+//        System.out.println(x + y);
+
+//        int x = 5;
+//        int y = 10;
+//        int z = x + y;
+//        System.out.println(z);
+
+//        int x = 5, y = 6, z = 50;
+//        System.out.println(x + y + z);
+//__________________________________________________________________
+        //Java Data Types 8, 9, 10
+//        int myNum = 9;
+//        float myFloatNum = 8.99f;
+//        char myLetter = 'A';
+//        boolean myBool = true;
+//        String myText = "Hello World";
+
+        //byte, short, int, long, float, double, boolean and char are called:
+//        primitive data types
+
+        //Type Casting
+//        double myDouble = 9.78d;
+//        int myInt = (int) myDouble;
+//        System.out.println(myInt);
+//__________________________________________________________________
+        //Java Operators 11,12,13,14
+//        System.out.println(10 * 5);
+//
+//        System.out.println(10 / 5);
+//
+//        int x = 10;
+//        ++x;
+
+//        int x = 10;
+//        x += 5;
+//__________________________________________________________________
+        //Java Strings
+//        String greeting = "Hello";
+
+//        String txt = "Hello";
+//        System.out.println(txt.length());
+
+//        String txt = "Hello";
+//        System.out.println(txt.toUpperCase());
+
+//        String firstName = "John ";
+//        String lastName = "Doe";
+//        System.out.println(firstName + lastName);
+
+//        String firstName = "John ";
+//        String lastName = "Doe";
+//        System.out.println(firstName.concat(lastName));
+
+//        String txt = "Hello Everybody";
+//        System.out.println(txt.indexOf("e"));
+//__________________________________________________________________
+        //Java math
+//        int x = 5;
+//        int y = 10;
+//        System.out.println(Math.max(x,y));
+
+//        int x = 16;
+//        System.out.println(Math.sqrt(x));
+
+//        System.out.println(Math.random());
+//__________________________________________________________________
+        //Java booleans
+//        boolean isJavaFun = true;
+//        boolean isFishTasty = false;
+//        System.out.println(isJavaFun);
+//        System.out.println(isFishTasty);
+
+        
+
+
+
+
+    }
+
+}
