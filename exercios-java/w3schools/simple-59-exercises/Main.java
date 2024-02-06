e07ad233c29bbbd8b756f2d7e048fdefe86fcbfd
Daniel <daniel10gja@gmail.com>
2024-02-05 23:56:04 -0300

    feat: mais exercicios feitos

--- exercios-java/w3schools/simple-59-exercises/Main.java
+++ exercios-java/w3schools/simple-59-exercises/Main.java
@@ -2,6 +2,41 @@
 //access this list in: w3schools.com/java/exercise.asp
 public class Main {
 
+//__________________________________________________________________
+        //Java methods
+//    Insert the missing part to call myMethod from main.
+//    static void myMethod() {
+//        System.out.println("I just got executed!");
+//    }
+
+//    Insert the missing part to call myMethod from main two times.
+//    static void myMethod() {
+//        System.out.println("I just got executed!");
+//    }
+
+//    Add a fname parameter of type String to myMethod, and output "John Doe".
+//        static void myMethod(String fname) {
+//            System.out.println(fname + " Doe");
+//        }
+
+//    Insert the missing part to print the number 8 in main, by using a specific keyword inside myMethod:
+//        static int myMethod(int x) {
+//            return 5 + x;
+//        }
+
+//    Follow the comments to insert the missing parts of the code below:
+//    Create a checkAge() method with an integer variable called age
+        static void checkAge(int age) {
+            // If age is less than 18, print "Access denied"
+            if (age < 18) {
+                System.out.println("Access denied");
+
+            // If age is greater than, or equal to, 18, print "Access granted"
+            } else {
+                System.out.println("Access granted");
+            }
+        }
+
     public static void main(String[] args) {
 
         //Java Sytax 1,2
@@ -89,11 +124,148 @@ public class Main {
 //        System.out.println(isJavaFun);
 //        System.out.println(isFishTasty);
 
-        
+//        int x = 10;
+//        int y = 9;
+//        System.out.println(y<x);
+//__________________________________________________________________
+        //Java If...Else
+//        int x = 50;
+//        int y = 10;
+//        if (x > y){
+//            System.out.println("Hello World");
+//        }
 
+//        int x = 50;
+//        int y = 50;
+//        if (x == y){
+//            System.out.println("Hellor World");
+//        }
 
+//        int x = 50;
+//        int y = 50;
+//        if (x == y) {
+//            System.out.println("Yes");
+//        }else{
+//            System.out.println("No");
+//        }
 
+//        int x = 50;
+//        int y = 50;
+//        if (x == y){
+//            System.out.println("1");
+//        } else if (x > y) {
+//            System.out.println("2");
+//        } else {
+//            System.out.println("3");
+//        }
 
+//        int time = 20;
+//        String result = (time < 18) ? "Good Day.": "Good Evening.";
+//        System.out.println(result);
+//__________________________________________________________________
+        //Java switch
+//        int day = 2;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Saturday");
+//                break;
+//            case 2:
+//                System.out.println("Sunday");
+//                break;
+//        }
+
+//        int day = 4;
+//        switch (day){
+//            case 1:
+//                System.out.println("Saturday");
+//                break;
+//            case 2:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Weekend");
+//        }
+//__________________________________________________________________
+        //Java Loops
+
+//        Print i as long as i is less than 6.
+//        int i = 1;
+//        while(i<6) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        Use the do/while loop to print i as long as i is less than 6.
+//        int i = 1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<6);
+
+//        Use a for loop to print "Yes" 5 times:
+//        for (int i=0;i<=4;i++) {
+//            System.out.println("Yes");
+//        }
+
+//        Loop through the items in the cars array.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+//        for (String i: cars) {
+//            System.out.println(i);
+//        }
+
+//        Stop the loop if i is 5.
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 5) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+//        In the loop, when the value is "4", jump directly to the next value.
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 4) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//__________________________________________________________________
+        //Java arrays
+//        Create an array of type String called cars.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+
+//        Print the second item in the cars array.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+//        System.out.println(cars[1]);
+
+//        Change the value from "Volvo" to "Opel", in the cars array.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+//        cars[0] = "Opel";
+//        System.out.println(cars[0]);
+
+//        Find out how many elements the cars array have.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+//        System.out.println(cars.length);
+
+//        Loop through the items in the cars array.
+//        String[] cars = {"Volvo", "BMW", "Ford"};
+//        for (String i: cars ) {
+//            System.out.println(i);
+//        }
+
+//        Insert the missing parts to create a two-dimensional array.
+//        int [][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+//__________________________________________________________________
+        //Java methods
+//        myMethod();
+
+//        myMethod();
+//        myMethod();
+
+//        myMethod("John");
+
+//        System.out.println(myMethod(3));
+
+        // Call the checkAge method and pass along an age of 20
+        checkAge(20);
     }
-
 }
