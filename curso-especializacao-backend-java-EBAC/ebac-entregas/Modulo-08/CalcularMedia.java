10d4abae9d4a1cb07b6606ee425e9b965653e7cc
Daniel <daniel10gja@gmail.com>
2024-02-07 23:40:51 -0300

    feat: calcularMedia de 4 notas

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/Modulo-08/CalcularMedia.java
@@ -0,0 +1,8 @@
+public class CalcularMedia {
+    public static void main(String[] args) {
+        int a=6, b=8, c=9, d=7, resultado;
+        resultado = ( (a+b+c+d) / 4);
+
+        System.out.println("A media final eh: " + resultado);
+    }
+}
