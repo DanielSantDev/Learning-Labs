e1ba2ac533d255fbd65e0112a35267c1e93fe160
Daniel <daniel10gja@gmail.com>
2024-02-08 23:56:12 -0300

    feat: forLoop

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-10/src/For.java
@@ -0,0 +1,10 @@
+public class For {
+
+    public static void main(String[] args) {
+        System.out.println("TABUADA DO 10");
+        for(int i=0; i <= 10 ; i++){
+            int a=10;
+            System.out.println("10 X " + i + " = " + (a *= i));
+        }
+    }
+}
