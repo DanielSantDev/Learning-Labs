3f237ca92baa34446a91b1108d70849da0b25d33
Daniel <daniel10gja@gmail.com>
2024-02-14 23:28:52 -0300

    feat: linked list 1

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-11/src/List/ExemploLinkedList01.java
@@ -0,0 +1,52 @@
+package List;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Daniel Santana
+ * implementa uma lista ligada
+ * cada nó contém o dado e uma referência para o próximo nó
+ * busca é linear e inserções e exclusões são rápidas
+ * prefira LinkedList quando a aplicação exigir grande quantidade de inserções e exclusões
+ * consume mais memoria
+ *
+ */
+
+public class ExemploLinkedList01 {
+
+    public static void main(String[] args) {
+        exemploListaSimples();
+        exemploListaSimplesOrdemAscendente();
+    }
+    private static void exemploListaSimples() {
+        System.out.println("****** exemploListaSimples ******");
+        List<String> lista = new LinkedList<>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        System.out.println(lista);
+        System.out.println("");
+
+        lista.remove(0);
+        System.out.println(lista);
+        boolean contem = lista.contains("Antonio Sousa");
+        System.out.println(contem);
+
+        System.out.println(lista.get(0));
+    }
+
+    private static void exemploListaSimplesOrdemAscendente() {
+        System.out.println("****** exemploListaSimplesOrdemAscendente ******");
+        List<String> lista = new LinkedList<String>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        Collections.sort(lista);
+        System.out.println(lista);
+        System.out.println("");
+    }
+
+}
