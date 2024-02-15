cd4eb99257bc22d6bb80983be779b111567d4c0a
Daniel <daniel10gja@gmail.com>
2024-02-14 23:29:11 -0300

    feat: teste de performance

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-11/src/List/TestePerformance.java
@@ -0,0 +1,82 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Daniel Santana
+ * Executa um teste de performance de ArrayList e LinkedList
+ *
+ */
+public class TestePerformance {
+
+    public static void main(String[] args) {
+        testeArrayListAdd();
+        testeLinkedListAdd();
+
+        testeArrayListRemove();
+        testeLinkedListRemove();
+    }
+
+    private static void testeLinkedListRemove() {
+        final int MAX = 20000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new LinkedList<>();
+        for (int i = 0; i < MAX; i++) {
+            lista.add(i);
+        }
+
+        for (int i = MAX -1; i > 0; i--) {
+            lista.remove(i);
+        }
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total LinkedList Remove: " + (tFim - tInicio));
+    }
+
+    private static void testeLinkedListAdd() {
+        final int MAX = 20000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new LinkedList<>();
+        for (int i = 0; i < MAX; i++) {
+            lista.add(i);
+        }
+
+        for (int i = 0; i < MAX; i++) {
+            lista.contains(i);
+        }
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total LinkedList ADD: " + (tFim - tInicio));
+    }
+
+    private static void testeArrayListRemove() {
+        final int MAX = 20000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new ArrayList<Integer>();
+        for (int i = 0; i < MAX; i++) {
+            lista.add(i);
+        }
+
+        for (int i = MAX -1; i > 0; i--) {
+            lista.remove(i);
+        }
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total ArrayList Remove: " + (tFim - tInicio));
+    }
+
+    private static void testeArrayListAdd() {
+        final int MAX = 20000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new ArrayList<Integer>();
+        for (int i = 0; i < MAX; i++) {
+            lista.add(i);
+        }
+
+        for (int i = 0; i < MAX; i++) {
+            lista.contains(i);
+        }
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total ArrayList ADD: " + (tFim - tInicio));
+    }
+}
