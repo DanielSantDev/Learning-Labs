ab1080887fe2330d1616ec6a9afbd9a45e03019e
Daniel <daniel10gja@gmail.com>
2024-02-17 22:47:05 -0300

    feat: main

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Interface/Main.java
@@ -0,0 +1,29 @@
+package Interface;
+
+public class Main {
+
+    public static void main(String args[]) {
+        ICaneta caneta = new CanetaEsferografica();
+        caneta.escrever("Olá Rodrigo");
+        caneta.escreverComumATodas();
+        System.out.println(caneta.getCor());
+
+        ICaneta giz = new Giz();
+        giz.escrever("Olá Rodrigo");
+        giz.escreverComumATodas();
+        System.out.println(giz.getCor());
+
+        Lapis lapis = new Lapis();
+        lapis.escrever("Olá Rodrigo");
+        lapis.escreverComumATodas();
+        System.out.println(lapis.getCor());
+
+        ICarro carro = new CarroPasseio();
+        carro.andar();
+        carro.parar();
+
+        ICarro caminhao = new Caminhao();
+        caminhao.andar();
+        caminhao.parar();
+    }
+}
