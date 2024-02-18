d9fb0358ab1ae5059924c68666e61c5ec372cfd6
Daniel <daniel10gja@gmail.com>
2024-02-18 18:19:24 -0300

    feat: comissionado class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Abstratas/Comissionado.java
@@ -0,0 +1,29 @@
+package Abstratas;
+
+public class Comissionado extends Empregado {
+
+    private Double totalVenda;
+
+    private Double totalComissao;
+
+    @Override
+    public Double vencimento() {
+        return totalVenda*totalComissao;
+    }
+
+    public Double getTotalVenda() {
+        return totalVenda;
+    }
+
+    public void setTotalVenda(Double totalVenda) {
+        this.totalVenda = totalVenda;
+    }
+
+    public Double getTotalComissao() {
+        return totalComissao;
+    }
+
+    public void setTotalComissao(Double totalComissao) {
+        this.totalComissao = totalComissao;
+    }
+}
