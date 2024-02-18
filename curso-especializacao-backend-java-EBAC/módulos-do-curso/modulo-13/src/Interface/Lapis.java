1e6a03fa02b563123372f5ec5972d73cbd4e60af
Daniel <daniel10gja@gmail.com>
2024-02-17 22:43:52 -0300

    feat: Lapis

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Interface/Lapis.java
@@ -0,0 +1,14 @@
+package Interface;
+
+public class Lapis implements ICaneta {
+
+    @Override
+    public void escrever(String texto) {
+        System.out.println("Escrevendo o valor " + texto + " na classe " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getCor() {
+        return "Preto claro";
+    }
+}
