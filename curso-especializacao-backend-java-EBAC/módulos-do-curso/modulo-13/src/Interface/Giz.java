12b1314d9a4f3e93829015cd5c11deb537d4133b
Daniel <daniel10gja@gmail.com>
2024-02-17 22:43:58 -0300

    feat: Giz

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Interface/Giz.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public class Giz implements ICaneta {
+    @Override
+    public void escrever(String texto) {
+        System.out.println("Escrevendo o valor " + texto + " na classe " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getCor() {
+        return "Branco";
+    }
+}
