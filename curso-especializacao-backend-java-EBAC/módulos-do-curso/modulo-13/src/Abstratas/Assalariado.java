67bae2abaf6bbb4a6ab53678c47fe15e7bdadb91
Daniel <daniel10gja@gmail.com>
2024-02-18 18:19:13 -0300

    feat: assalariado class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Abstratas/Assalariado.java
@@ -0,0 +1,19 @@
+package Abstratas;
+
+public class Assalariado extends Empregado {
+
+    private Double salario;
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public Double vencimento() {
+        return salario;
+    }
+}
