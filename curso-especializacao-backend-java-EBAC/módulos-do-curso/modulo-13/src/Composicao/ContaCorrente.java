fae91db71cbdcecf4002d848ec6b16df6bebb0a2
Daniel <daniel10gja@gmail.com>
2024-02-18 22:26:39 -0300

    feat: ContaCorrente class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Composicao/ContaCorrente.java
@@ -0,0 +1,34 @@
+package Composicao;
+
+public class ContaCorrente {
+
+    private Banco banco;
+
+    private Double saldo;
+
+    private Double chequeEspecial;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(Double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+}
