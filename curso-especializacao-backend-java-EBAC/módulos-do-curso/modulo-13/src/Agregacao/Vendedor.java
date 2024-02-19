dff1a179a672d266b12d81dbaf97b62f90c1dee7
Daniel <daniel10gja@gmail.com>
2024-02-18 22:23:04 -0300

    feat: Vendedor class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Agregacao/Vendedor.java
@@ -0,0 +1,28 @@
+package Agregacao;
+
+public class Vendedor {
+
+    private String nome;
+
+    private Double comissao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(Double comissao) {
+        this.comissao = comissao;
+    }
+
+    public void vende() {
+        System.out.println("Vender");
+    }
+}
