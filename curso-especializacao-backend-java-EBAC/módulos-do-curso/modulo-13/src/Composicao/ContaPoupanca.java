9e81ba984d97bec3c0c5e926b702f617f70a32ff
Daniel <daniel10gja@gmail.com>
2024-02-18 22:26:03 -0300

    feat: ContaPoupanca class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-13/src/Composicao/ContaPoupanca.java
@@ -0,0 +1,24 @@
+package Composicao;
+
+public class ContaPoupanca {
+
+    private Banco banco;
+
+    private Double saldo;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+}
