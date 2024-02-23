287d868eac33f1891435c46875e63d53f0986d0f
Daniel <daniel10gja@gmail.com>
2024-02-22 23:30:00 -0300

    feat: new interface

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-14/CadastroClienteSwing/src/dao/IClienteDAO.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-14/CadastroClienteSwing/src/dao/IClienteDAO.java
@@ -9,9 +9,11 @@ public interface IClienteDAO {
     public Boolean cadastrar(Cliente cliente);
 
     public void excluir(Long cpf);
+    
+    public void atualizar(Cliente cliente);
 
     public void alterar(Cliente cliente);
-
+    
     public Cliente consultar(Long cpf);
 
     public Collection<Cliente> buscarTodos();
