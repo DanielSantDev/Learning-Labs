c52c6a33a2f1f29b4053c593f06f2c2b04687286
Daniel <daniel10gja@gmail.com>
2024-02-24 22:11:42 -0300

    feat: singleton

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/Singleton.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
