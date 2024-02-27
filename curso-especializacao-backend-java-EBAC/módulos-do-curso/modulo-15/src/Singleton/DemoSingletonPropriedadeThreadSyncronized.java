e94112e468a3b9d473d7668a3ad88ca79d27fb3a
Daniel <daniel10gja@gmail.com>
2024-02-26 21:51:02 -0300

    feat: DemoSingletonThredSync

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/DemoSingletonPropriedadeThreadSyncronized.java
@@ -0,0 +1,27 @@
+package Singleton;
+
+public class DemoSingletonPropriedadeThreadSyncronized {
+
+    public static void main(String args[]) {
+        Thread t1 = new Thread(new ThreadFoo());
+        Thread t2 = new Thread(new ThreadBar());
+        t1.start();
+        t2.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            SingletonPropriedadeSyncronized singleton = SingletonPropriedadeSyncronized.getInstance("Teste");
+            System.out.println(singleton.getValue());
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            SingletonPropriedadeSyncronized singleton = SingletonPropriedadeSyncronized.getInstance("Teste1");
+            System.out.println(singleton.getValue());
+        }
+    }
+}
