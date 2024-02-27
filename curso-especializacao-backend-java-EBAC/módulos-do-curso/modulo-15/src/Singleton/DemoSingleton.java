28bf76f15df334abfab695800f36d37fa1181b4e
Daniel <daniel10gja@gmail.com>
2024-02-26 21:49:06 -0300

    feat: demonSingleton

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/DemoSingleton.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class DemoSingleton {
+
+    public static void main(String args[]) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println(singleton);
+        System.out.println(singleton1);
+    }
+}
