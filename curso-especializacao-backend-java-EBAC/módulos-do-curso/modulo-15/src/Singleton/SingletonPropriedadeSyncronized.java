79b3489d6c4803f680befd0a898f6e1d7d4a5ebc
Daniel <daniel10gja@gmail.com>
2024-02-26 21:59:13 -0300

    feat: SingletonPropSync class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/SingletonPropriedadeSyncronized.java
@@ -0,0 +1,22 @@
+package Singleton;
+
+public class SingletonPropriedadeSyncronized {
+
+    private static SingletonPropriedadeSyncronized singleton;
+    private String value;
+
+    private SingletonPropriedadeSyncronized(String value) {
+        this.value = value;
+    }
+
+    public static synchronized SingletonPropriedadeSyncronized getInstance(String value) {
+        if (singleton == null) {
+            singleton = new SingletonPropriedadeSyncronized(value);
+        }
+        return singleton;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
