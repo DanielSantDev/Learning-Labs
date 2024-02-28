40213b765a1a6f33fce98cad3b188cfcabf65f54
Daniel <daniel10gja@gmail.com>
2024-02-27 21:44:38 -0300

    feat: Gerente class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Builder/Gerente.java
@@ -0,0 +1,27 @@
+package Builder;
+
+public class Gerente {
+
+    BurgerBuilder builder;
+
+//    public Gerente(BurgerBuilder builder) {
+//        this.builder = builder;
+//    }
+
+    public BurgerBuilder getBuilder() {
+        return builder;
+    }
+
+    public void setBuilder(BurgerBuilder builder) {
+        this.builder = builder;
+    }
+
+    public Burger buildBurger() {
+        builder.buildBun();
+        builder.buildMeat();
+        builder.buildSalad();
+        builder.buildCheese();
+        builder.buildSauce();
+        return builder.build();
+    }
+}
