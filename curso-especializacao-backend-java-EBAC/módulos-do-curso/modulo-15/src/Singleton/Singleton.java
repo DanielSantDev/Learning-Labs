59cfd4de179c1aff92f449afe74d56221b1515de
Daniel <daniel10gja@gmail.com>
2024-02-27 21:28:57 -0300

    comments

--- curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/Singleton.java
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Singleton/Singleton.java
@@ -1,4 +1,8 @@
 package Singleton;
+/*
+*  O padrão Singleton é projetado para garantir que uma classe tenha apenas uma instância e fornecer um ponto global
+* para acessá-la. Uma vez que a instância única tenha sido criada, tentativas subsequentes de criar novas instâncias
+* resultarão na devolução da instância já existente.* */
 
 public class Singleton {
 
@@ -9,6 +13,7 @@ public class Singleton {
     }
 
     public static Singleton getInstance() {
+        //ñ pode ser mais instanciado dps de ser instanciado pela primeira vez
         if (singleton == null) {
             singleton = new Singleton();
         }
