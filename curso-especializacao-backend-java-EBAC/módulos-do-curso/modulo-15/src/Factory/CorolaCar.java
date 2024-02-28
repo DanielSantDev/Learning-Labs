d5e4a159a1b2288f33809425962405d3a4a74d7a
Daniel <daniel10gja@gmail.com>
2024-02-27 22:29:04 -0300

    feat: CorolaCar class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Factory/CorolaCar.java
@@ -0,0 +1,8 @@
+package Factory;
+
+public class CorolaCar extends Car{
+
+    public CorolaCar(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
