9c7d6b77a24ef2d67ec94333727dafc6f2b4710e
Daniel <daniel10gja@gmail.com>
2024-02-27 22:29:46 -0300

    feat: Demo class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Factory/Demo.java
@@ -0,0 +1,19 @@
+package Factory;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Customer cliente = new Customer("A", false);
+        Factory factory = getFactory(cliente);
+        Car car = factory.create(cliente.getGradeRequest());
+        car.startEngine();
+    }
+
+    private static Factory getFactory(Customer cliente) {
+        if (cliente.hasCompanyContract()) {
+            return new ContratosFactory();
+        } else {
+            return new SemContratosFactory();
+        }
+    }
+}
