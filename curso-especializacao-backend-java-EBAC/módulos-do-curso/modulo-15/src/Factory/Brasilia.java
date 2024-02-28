1d262693b22fa1dd26dc32f45f79bf9734ab4a35
Daniel <daniel10gja@gmail.com>
2024-02-27 22:28:11 -0300

    feat: Factory class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-15/src/Factory/Brasilia.java
@@ -0,0 +1,7 @@
+package Factory;
+
+public class Brasilia extends Car{
+    public Brasilia(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
