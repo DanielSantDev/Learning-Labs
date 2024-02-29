feecb20c02036a863be5e446626499e65abccc50
Daniel <daniel10gja@gmail.com>
2024-02-29 00:30:00 -0300

    feat: DropBoxAdapter class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-16/src/Adapter/DropBoxAdapter.java
@@ -0,0 +1,24 @@
+package Adapter;
+
+import java.io.File;
+
+public class DropBoxAdapter implements PersistenciaArquivos {
+
+    private DropBox dropBox;
+
+    public DropBoxAdapter(DropBox dropbox) {
+        this.dropBox = dropbox;
+    }
+
+
+    @Override
+    public void gravar(File file) {
+        dropBox.download(file.getAbsolutePath());
+    }
+
+    @Override
+    public File ler(String caminho) {
+        DropBoxFile file = dropBox.download(caminho);
+        return new File(file.getLocalPath());
+    }
+}
