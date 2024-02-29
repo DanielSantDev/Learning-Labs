7f4bcd0927d399a2c063510b6a13b601f3ce9605
Daniel <daniel10gja@gmail.com>
2024-02-28 23:13:35 -0300

    feat: Policial class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-16/src/TemplateMethod/Policial.java
@@ -0,0 +1,14 @@
+package TemplateMethod;
+
+public class Policial extends Trabalhador {
+
+    @Override
+    protected void trabalhar() {
+        System.out.println("Trabalhando como Policial");
+    }
+
+    @Override
+    protected void levantar() {
+        System.out.println("Levantando da cama as 08");
+    }
+}
