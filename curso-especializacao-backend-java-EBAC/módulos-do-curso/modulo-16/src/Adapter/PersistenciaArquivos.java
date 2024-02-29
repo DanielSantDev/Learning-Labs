8519d7e77726ec119431bfec71131137824b985a
Daniel <daniel10gja@gmail.com>
2024-02-29 00:30:36 -0300

    feat: PersistenciaArquivos class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-16/src/Adapter/PersistenciaArquivos.java
@@ -0,0 +1,11 @@
+package Adapter;
+
+import java.io.File;
+
+public interface PersistenciaArquivos {
+
+    //byte[] bytes = Files.readAllBytes(file.toPath());
+    public void gravar(File file);
+
+    public File ler(String caminho);
+}
