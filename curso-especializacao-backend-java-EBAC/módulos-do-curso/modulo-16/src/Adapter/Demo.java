95256b414edcb5f3ddcc52e71b2308b2bb66ebaa
Daniel <daniel10gja@gmail.com>
2024-02-29 00:29:46 -0300

    feat: Demo class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-16/src/Adapter/Demo.java
@@ -0,0 +1,13 @@
+package Adapter;
+
+import java.io.File;
+
+public class Demo {
+
+    public static void main(String args[]) {
+        String file = "teste.txt";
+        DropBox drop = new DropBox();
+        PersistenciaArquivos arquivos = new DropBoxAdapter(drop);
+        arquivos.gravar(new File(file));
+    }
+}
