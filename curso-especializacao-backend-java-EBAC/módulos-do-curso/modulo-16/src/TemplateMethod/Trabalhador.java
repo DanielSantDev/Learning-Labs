e7f2c1e5aa4b4f8baca37daecf947d7ca4261fce
Daniel <daniel10gja@gmail.com>
2024-02-28 23:12:44 -0300

    feat: Trabalhador class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-16/src/TemplateMethod/Trabalhador.java
@@ -0,0 +1,30 @@
+package TemplateMethod;
+
+public abstract class Trabalhador {
+
+    public void executar() {
+        iniciarRotina();
+        levantar();
+        irAoTrabalho();
+        trabalhar();
+        voltarParaCasa();
+    }
+
+    protected abstract void trabalhar();
+
+    protected void voltarParaCasa() {
+        System.out.println("Voltando para casa");
+    }
+
+    protected void irAoTrabalho() {
+        System.out.println("Indo trabalhar");
+    }
+
+    protected void iniciarRotina() {
+        System.out.println("Iniciando Rotina");
+    }
+
+    protected void levantar() {
+        System.out.println("Levantando da cama");
+    }
+}
