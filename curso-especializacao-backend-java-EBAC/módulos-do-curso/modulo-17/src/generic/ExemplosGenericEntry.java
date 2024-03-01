c628a659dda443709ff31256d43e8548aee46daf
Daniel <daniel10gja@gmail.com>
2024-02-29 22:34:48 -0300

    feat: ExemplosGenericsEntry class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/generic/ExemplosGenericEntry.java
@@ -0,0 +1,12 @@
+package generic;
+
+public class ExemplosGenericEntry {
+
+    public static void main(String args[]) {
+        GenericEntry<String, Long> entry = new GenericEntry<String, Long>("Teste", 10L);
+        System.out.println(entry.getData()+ " " + entry.getCodigo());
+
+        GenericEntry<Long, Integer> entryLong = new GenericEntry<Long, Integer>(10L, 11);
+        System.out.println(entryLong.getData() + " " + entryLong.getCodigo());
+    }
+}
