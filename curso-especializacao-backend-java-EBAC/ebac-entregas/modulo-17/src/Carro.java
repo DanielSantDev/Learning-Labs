2292c229e88183cacf0f84f74c789e7f1bc4ebc0
Daniel <daniel10gja@gmail.com>
2024-02-29 22:18:01 -0300

    feat: Carro abstract class

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-17/src/Carro.java
@@ -0,0 +1,22 @@
+public abstract class Carro {
+    private String marca;
+    private String modelo;
+
+    public Carro(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca do Carro: " + marca + "\tModelo do Carro: " + modelo;
+    }
+}
