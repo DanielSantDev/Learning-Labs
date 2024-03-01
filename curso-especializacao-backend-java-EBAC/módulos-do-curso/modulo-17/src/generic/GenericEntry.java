1559427ca9e2ed679fc426c4b16a7a9b233a344e
Daniel <daniel10gja@gmail.com>
2024-02-29 22:35:56 -0300

    feat: GenericEntry class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/generic/GenericEntry.java
@@ -0,0 +1,21 @@
+package generic;
+
+public class GenericEntry<T, E> {
+
+    private T data;
+
+    private E codigo;
+
+    public GenericEntry(T data, E codigo) {
+        this.data = data;
+        this.codigo = codigo;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public E getCodigo() {
+        return this.codigo;
+    }
+}
