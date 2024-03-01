90acf9ff11faca91659dc02afdcfd42bd998a477
Daniel <daniel10gja@gmail.com>
2024-02-29 22:32:08 -0300

    docs: explicação da Classe ListaDeCarros

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-17/src/ListaDeCarros.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-17/src/ListaDeCarros.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaDeCarros<T extends Carro> {
+
+    /**
+     * @author DanielSantDev
+     *
+     * A classe ListaDeCarros<T extends Carro> é um exemplo de Generics.
+     * Ela é parametrizada com T, que é uma extensão da classe Carro.
+     * Isso significa que a lista ListaDeCarros pode conter qualquer tipo de carro ou subtipo de Carro.
+     *
+     * A utilização de Generics nesse contexto permite que a classe ListaDeCarros seja flexível o suficiente para -
+     * trabalhar com diferentes tipos de carros sem sacrificar a segurança de tipos.
+     *
+     */
     private List<T> listaDeCarros = new ArrayList<>();
 
     public void adicionarCarro(T carro) {
