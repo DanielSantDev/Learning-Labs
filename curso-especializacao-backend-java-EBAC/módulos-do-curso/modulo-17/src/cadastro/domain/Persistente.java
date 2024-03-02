c9a9dcac2e616919e772e8c03c3cd04bbb5cdb11
Daniel <daniel10gja@gmail.com>
2024-03-01 21:15:28 -0300

    feat: domain package

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/cadastro/domain/Persistente.java
@@ -0,0 +1,10 @@
+package cadastro.domain;
+
+/**
+ *
+ * Classe que representa todas as entidades ou objetos da aplicação que seram salvas no banco de dados
+ */
+public interface Persistente {
+
+    public Long getCodigo();
+}
