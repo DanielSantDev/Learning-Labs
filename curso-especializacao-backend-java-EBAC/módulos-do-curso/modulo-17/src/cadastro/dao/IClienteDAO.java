78f3750283e5e6833cc85d83f9ac76ddc969c0c5
Daniel <daniel10gja@gmail.com>
2024-03-01 21:15:06 -0300

    feat: dao

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/cadastro/dao/IClienteDAO.java
@@ -0,0 +1,22 @@
+package cadastro.dao;
+
+import cadastro.dao.generic.IGenericDAO;
+import cadastro.domain.Cliente;
+
+import java.util.Collection;
+
+/**
+ * Interface que representa os dados de Cliente
+ */
+public interface IClienteDAO extends IGenericDAO<Cliente> {
+
+//    public Boolean cadastrar(Cliente cliente);
+//
+//    public void excluir(Long cpf);
+//
+//    public void alterar(Cliente cliente);
+//
+//    public Cliente consultar(Long cpf);
+//
+//    public Collection<Cliente> buscarTodos();
+}
---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/cadastro/dao/IProdutoDAO.java
@@ -0,0 +1,14 @@
+package cadastro.dao;
+
+import cadastro.dao.generic.IGenericDAO;
+import cadastro.domain.Cliente;
+import cadastro.domain.Produto;
+
+import java.util.Collection;
+
+/**
+ * Interface que representa os dados de Produto
+ */
+public interface IProdutoDAO extends IGenericDAO<Produto> {
+
+}
