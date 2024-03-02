d867a2fd9d29e10dbcc2e84e6b903b70476605e8
Daniel <daniel10gja@gmail.com>
2024-03-01 21:16:36 -0300

    feat: SingletonMap Class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-17/src/cadastro/SingletonMap.java
@@ -0,0 +1,40 @@
+package cadastro;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * Classe Singleton que garante que o <code>map</code> será único em toda a vita da aplicação.
+ *
+ */
+public class SingletonMap {
+
+    private static SingletonMap singletonMap;
+
+    /**
+     * Contém todos os registros da aplicação.
+     * Simula o banco de dados
+     */
+    protected Map<Class, Map<Long, ?>> map;
+
+    private SingletonMap() {
+        map = new HashMap<>();
+    }
+
+    /**
+     * Método que garante o retorno de apenas uma instância desse objeto
+     *
+     * @return SingletonMap
+     */
+    public static SingletonMap getInstance() {
+        if (singletonMap == null) {
+            singletonMap = new SingletonMap();
+        }
+        return singletonMap;
+    }
+
+    public Map<Class, Map<Long, ?>> getMap() {
+        return this.map;
+    }
+}
