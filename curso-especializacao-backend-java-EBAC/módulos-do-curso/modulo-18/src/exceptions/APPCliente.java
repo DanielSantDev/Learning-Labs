de61fd43fae091b2a798a4a6d94cf2c225a86c3c
Daniel <daniel10gja@gmail.com>
2024-03-03 19:23:35 -0300

    feat: AppCliente Class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-18/src/exceptions/APPCliente.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class APPCliente {
+
+    public static void main(String args[]) {
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite o código do cliente",
+                "", JOptionPane.INFORMATION_MESSAGE);
+
+        try {
+            ClienteService.consutlarCliente(opcao);
+        } catch (ClienteNaoEncontrato2Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Sair",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+//        try {
+//            ClienteService.consutlarCliente(opcao);
+//        } catch(ClienteNaoEncontrato2Exception e) {
+//            JOptionPane.showMessageDialog(null, e.getMessage(), "Sair",JOptionPane.INFORMATION_MESSAGE);
+//        }
+
+    }
+}
