482c7d71f5aa8811bfca7cc4f041844d31113c11
Daniel <daniel10gja@gmail.com>
2024-03-03 19:26:16 -0300

    feat: ClienteNaoEncontrado2Exception Class

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-18/src/exceptions/ClienteNaoEncontrato2Exception.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ClienteNaoEncontrato2Exception extends Exception{
+
+    public ClienteNaoEncontrato2Exception(String msg, Throwable e) {
+        super(msg,e);
+    }
+}
