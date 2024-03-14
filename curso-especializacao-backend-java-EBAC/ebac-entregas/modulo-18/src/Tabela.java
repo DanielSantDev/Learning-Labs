386db4c3cfafe19f6115c3a5454e547ffae63be4
Daniel <daniel10gja@gmail.com>
2024-03-13 23:41:16 -0300

    feat: Tabela Annotation Class

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-18/src/Tabela.java
@@ -0,0 +1,9 @@
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE, ElementType.CONSTRUCTOR})
+public @interface Tabela {
+    String nomeTabela();
+
+}
