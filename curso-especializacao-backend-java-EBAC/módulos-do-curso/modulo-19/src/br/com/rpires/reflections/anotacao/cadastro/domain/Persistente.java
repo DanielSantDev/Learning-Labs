353c734ef27617e29196f087b183e28c21e42956
Daniel <daniel10gja@gmail.com>
2024-05-07 00:05:52 -0300

    Create Persistente.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-19/src/br/com/rpires/reflections/anotacao/cadastro/domain/Persistente.java
@@ -0,0 +1,9 @@
+package br.com.rpires.reflections.anotacao.cadastro.domain;
+
+/**
+ * Classe que representa todas as entidades ou objetos da aplicação que seram salvas no banco de dados
+ */
+public interface Persistente {
+
+    //public Long getCodigo();
+}
