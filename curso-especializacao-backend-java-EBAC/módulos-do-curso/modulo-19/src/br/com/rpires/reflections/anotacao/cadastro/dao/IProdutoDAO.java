8ee0a5e0bac30eb8b30dc7cbf2b86db5869b1c8f
Daniel <daniel10gja@gmail.com>
2024-05-07 00:06:04 -0300

    Create IProdutoDAO.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-19/src/br/com/rpires/reflections/anotacao/cadastro/dao/IProdutoDAO.java
@@ -0,0 +1,12 @@
+package br.com.rpires.reflections.anotacao.cadastro.dao;
+
+import br.com.rpires.reflections.anotacao.cadastro.dao.generic.IGenericDAO;
+import br.com.rpires.reflections.anotacao.cadastro.domain.Produto;
+
+/**
+ *
+ * Interface que representa os dados de Produto
+ */
+public interface IProdutoDAO extends IGenericDAO<Produto> {
+
+}
