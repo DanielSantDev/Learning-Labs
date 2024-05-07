3813091c290488a79d397cb374b254a22cfdb731
Daniel <daniel10gja@gmail.com>
2024-05-07 00:04:30 -0300

    Create TipoChaveNaoEncontradaException.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-19/src/br/com/rpires/reflections/anotacao/cadastro/exception/TipoChaveNaoEncontradaException.java
@@ -0,0 +1,12 @@
+package br.com.rpires.reflections.anotacao.cadastro.exception;
+
+public class TipoChaveNaoEncontradaException extends Exception {
+
+    public TipoChaveNaoEncontradaException(String msg) {
+        this(msg, null);
+    }
+
+    public TipoChaveNaoEncontradaException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
