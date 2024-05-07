623dd6c2499f542a65d6fae56f4e3ad943ccca2d
Daniel <daniel10gja@gmail.com>
2024-05-07 00:04:36 -0300

    Create DadosInvalidosException.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-19/src/br/com/rpires/reflections/anotacao/cadastro/exception/DadosInvalidosException.java
@@ -0,0 +1,13 @@
+package br.com.rpires.reflections.anotacao.cadastro.exception;
+
+
+public class DadosInvalidosException extends Exception {
+
+    public DadosInvalidosException(String msg) {
+        this(msg, null);
+    }
+
+    public DadosInvalidosException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
