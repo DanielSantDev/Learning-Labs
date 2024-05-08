05ba070a9e7263f71c5a224f87b6c4859852be8e
Daniel <daniel10gja@gmail.com>
2024-05-07 23:24:32 -0300

    Create TesteLambdasThreads.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-20/src/br/com/danielsantdev/lambdas/TesteLambdasThreads.java
@@ -0,0 +1,21 @@
+package br.com.danielsantdev.lambdas;
+
+
+public class TesteLambdasThreads {
+
+    public static void main(String[] args) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Executanto comum");
+            }
+        };
+        new Thread(r).start();
+
+        Runnable rLambda = () -> System.out.println("Executanto rLambda");
+        new Thread(rLambda).start();
+
+        new Thread(() -> System.out.println("Executanto Lambda 1")).start();
+
+    }
+}
