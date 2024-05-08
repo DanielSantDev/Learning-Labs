29b29199ff9e0bab5fb975522e3d0e7f93eb4c51
Daniel <daniel10gja@gmail.com>
2024-05-07 23:23:59 -0300

    Create AnonimasTeste.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-20/src/br/com/danielsantdev/lambdas/anonimas/AnonimasTeste.java
@@ -0,0 +1,28 @@
+package br.com.danielsantdev.lambdas.anonimas;
+
+import br.com.danielsantdev.lambdas.TesteLambdasThreads;
+
+public class AnonimasTeste {
+
+    public static void main(String[] args) {
+        MyEventConsumer anonima = new MyEventConsumer() {
+            @Override
+            public void consumer(Object value) {
+                System.out.println(value);
+            }
+        };
+        anonima.consumer("Teste anônima");
+
+
+        MyEventConsumer anonimaLambda = (Object value) -> System.out.println(value);
+        anonimaLambda.consumer(10);
+
+        receberInterface("Value", value -> System.out.println(value));
+
+        receberInterface(Boolean.TRUE, value -> System.out.println(value));
+    }
+
+    private static void receberInterface(Object o, MyEventConsumer anonimaLambda) {
+        anonimaLambda.consumer(o);
+    }
+}
