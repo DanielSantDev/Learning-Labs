d244f2ff8217ea47555f18e44a61a9e4f54ba41c
Daniel <daniel10gja@gmail.com>
2024-05-09 15:54:08 -0300

    Create ExemplosSorted.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-22/src/br/com/danielsantdev/ExemplosSorted.java
@@ -0,0 +1,24 @@
+package br.com.danielsantdev;
+
+import br.com.danielsantdev.Pessoa;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ExemplosSorted {
+
+    public static void main(String[] args) {
+        List<Pessoa> lista = new Pessoa().populaPessoas();
+
+        Stream<Pessoa> stream = lista.stream()
+                .filter(pessoa -> pessoa.getNacionalidade().equals("Brasil"))
+                .sorted(Comparator.comparing(Pessoa::getNome));
+
+        Stream<Pessoa> stream1 = lista.stream()
+                .sorted(Comparator.comparing(Pessoa::getIdade));
+
+        Stream<Pessoa> stream2 = lista.stream()
+                .sorted((p1,p2) -> Integer.compare(p2.getIdade(), p1.getIdade()));
+    }
+}
