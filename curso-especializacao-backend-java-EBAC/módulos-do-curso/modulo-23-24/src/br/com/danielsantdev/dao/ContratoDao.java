064bb549142b1359b08304a753803c2d93bdcfd6
Daniel <daniel10gja@gmail.com>
2024-05-11 23:24:28 -0300

    Create ContratoDao.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-23-24/src/br/com/danielsantdev/dao/ContratoDao.java
@@ -0,0 +1,9 @@
+package br.com.danielsantdev.dao;
+
+public class ContratoDao implements IContratoDao {
+
+    @Override
+    public void salvar() {
+        throw new UnsupportedOperationException("Não funciona com o banco");
+    }
+}
