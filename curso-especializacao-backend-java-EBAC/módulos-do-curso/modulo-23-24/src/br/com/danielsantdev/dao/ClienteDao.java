52e743c328e52a103cc177459ee79d7ebfb63349
Daniel <daniel10gja@gmail.com>
2024-05-11 23:24:18 -0300

    Create ClienteDao.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-23-24/src/br/com/danielsantdev/dao/ClienteDao.java
@@ -0,0 +1,9 @@
+package br.com.danielsantdev.dao;
+
+public class ClienteDao implements IClienteDao {
+
+    public String salvar() {
+        throw new UnsupportedOperationException("Não funciona sem config de banco");
+        //return "Sucesso";
+    }
+}
