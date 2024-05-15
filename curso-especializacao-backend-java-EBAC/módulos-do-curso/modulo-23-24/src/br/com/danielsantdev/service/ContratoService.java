335ad35335e42ad7eb016ecd51b8c957fb552617
Daniel <daniel10gja@gmail.com>
2024-05-15 01:11:20 -0300

    Create ContratoService.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-23-24/src/br/com/danielsantdev/service/ContratoService.java
@@ -0,0 +1,19 @@
+package br.com.danielsantdev.service;
+
+import br.com.danielsantdev.dao.IContratoDao;
+
+
+public class ContratoService implements IContratoService {
+
+    private IContratoDao contratoDao;
+
+    public ContratoService(IContratoDao dao) {
+        this.contratoDao = dao;
+    }
+
+    @Override
+    public String salvar() {
+        contratoDao.salvar();
+        return "Sucesso";
+    }
+}
