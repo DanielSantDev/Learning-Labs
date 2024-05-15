83d80b3386eb1993081b34753e2061bd50c7ddab
Daniel <daniel10gja@gmail.com>
2024-05-14 23:46:54 -0300

    Create ContratoDaoMock.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-23-24/src/br/com/danielsantdev/dao/mocks/ContratoDaoMock.java
@@ -0,0 +1,11 @@
+package br.com.danielsantdev.dao.mocks;
+
+import br.com.danielsantdev.dao.IContratoDao;
+
+public class ContratoDaoMock implements IContratoDao {
+
+    @Override
+    public void salvar() {
+
+    }
+}
