dfe26b7b853e7ada9a10d850501591f42f1d8cb9
Daniel <daniel10gja@gmail.com>
2024-05-27 19:24:58 -0300

    Create TipoChave.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/main/java/annotation/TipoChave.java
@@ -0,0 +1,11 @@
+package main.java.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TipoChave {
+
+    String value();
+}
