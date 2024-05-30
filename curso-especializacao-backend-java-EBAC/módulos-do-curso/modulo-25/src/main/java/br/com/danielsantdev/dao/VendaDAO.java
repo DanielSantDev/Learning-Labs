3ee3e474e27f56e6bd269422475fd7f680053a9e
Daniel <daniel10gja@gmail.com>
2024-05-30 19:31:07 -0300

    Create VendaDAO.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/main/java/br/com/danielsantdev/dao/VendaDAO.java
@@ -0,0 +1,34 @@
+package main.java.br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.GenericDAO;
+import br.com.danielsantdev.domain.Venda;
+import br.com.danielsantdev.domain.Venda.Status;
+import br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+
+public class VendaDAO extends GenericDAO<Venda, String> implements IVendaDAO {
+
+	@Override
+	public Class<Venda> getTipoClasse() {
+		return Venda.class;
+	}
+
+	@Override
+	public void atualiarDados(Venda entity, Venda entityCadastrado) {
+		entityCadastrado.setCodigo(entity.getCodigo());
+		entityCadastrado.setStatus(entity.getStatus());
+	}
+
+	@Override
+	public void excluir(String valor) {
+		throw new UnsupportedOperationException("OPERAÇÃO NÃO PERMITIDA");
+	}
+
+	@Override
+	public void finalizarVenda(Venda venda) throws TipoChaveNaoEncontradaException {
+		venda.setStatus(Status.CONCLUIDA);
+		super.alterar(venda);
+	}
+	
+	
+
+}
