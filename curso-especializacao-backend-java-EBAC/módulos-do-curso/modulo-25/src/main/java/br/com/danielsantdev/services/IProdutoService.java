02910661beb533398008efd5e0d5b872531112b5
Daniel <daniel10gja@gmail.com>
2024-06-05 19:53:52 -0300

    Create IProdutoService.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/main/java/br/com/danielsantdev/services/IProdutoService.java
@@ -0,0 +1,9 @@
+package main.java.br.com.danielsantdev.services;
+
+import main.java.br.com.danielsantdev.domain.Produto;
+import main.java.br.com.danielsantdev.services.generic.IGenericService;
+
+
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
