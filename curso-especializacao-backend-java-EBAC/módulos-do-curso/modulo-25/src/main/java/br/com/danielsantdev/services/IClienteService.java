565ba0a271bdbef6e7a2db2c669e2e019e735669
Daniel <daniel10gja@gmail.com>
2024-06-05 19:53:47 -0300

    Create IClienteService.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/main/java/br/com/danielsantdev/services/IClienteService.java
@@ -0,0 +1,17 @@
+package main.java.br.com.danielsantdev.services;
+
+import main.java.br.com.danielsantdev.domain.Cliente;
+import main.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+
+
+public interface IClienteService {
+
+	Boolean cadastrar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+
+	Cliente buscarPorCPF(Long cpf);
+
+	void excluir(Long cpf);
+
+	void alterar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+
+}
