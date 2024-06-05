9e1c514ce71c46852001e11965813ef101f72f50
Daniel <daniel10gja@gmail.com>
2024-06-04 23:27:04 -0300

    Create ClienteService.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/main/java/br/com/danielsantdev/services/ClienteService.java
@@ -0,0 +1,37 @@
+package main.java.br.com.danielsantdev.services;
+
+import main.java.br.com.danielsantdev.dao.IClienteDAO;
+import main.java.br.com.danielsantdev.domain.Cliente;
+import main.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+import main.java.br.com.danielsantdev.services.generic.GenericService;
+
+public class ClienteService extends GenericService<Cliente, Long> implements IClienteService {
+	
+	//private IClienteDAO clienteDAO;
+	
+	public ClienteService(IClienteDAO clienteDAO) {
+		super(clienteDAO);
+		//this.clienteDAO = clienteDAO;
+	}
+
+//	@Override
+//	public Boolean salvar(Cliente cliente) throws TipoChaveNaoEncontradaException {
+//		return clienteDAO.cadastrar(cliente);
+//	}
+
+	@Override
+	public Cliente buscarPorCPF(Long cpf) {
+		return this.dao.consultar(cpf);
+	}
+
+//	@Override
+//	public void excluir(Long cpf) {
+//		clienteDAO.excluir(cpf);
+//	}
+//
+//	@Override
+//	public void alterar(Cliente cliente) throws TipoChaveNaoEncontradaException{
+//		clienteDAO.alterar(cliente);
+//	}
+
+}
