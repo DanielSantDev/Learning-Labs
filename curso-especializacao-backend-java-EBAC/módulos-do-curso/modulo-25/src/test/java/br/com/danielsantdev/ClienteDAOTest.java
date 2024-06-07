53110b1888b75886d64ebd074e14a8932c413a25
Daniel <daniel10gja@gmail.com>
2024-06-06 22:14:32 -0300

    Create ClienteDAOTest.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/test/java/br/com/danielsantdev/ClienteDAOTest.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package test.java.br.com.danielsantdev;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.rpires.dao.ClienteDAO;
+import br.com.rpires.dao.IClienteDAO;
+import br.com.rpires.domain.Cliente;
+import br.com.rpires.exceptions.TipoChaveNaoEncontradaException;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+public class ClienteDAOTest {
+	
+	private IClienteDAO clienteDao;
+
+	private Cliente cliente;
+	
+	public ClienteDAOTest() {
+		clienteDao = new ClienteDAO();
+	}
+	
+	@Before
+	public void init() throws TipoChaveNaoEncontradaException {
+		cliente = new Cliente();
+		cliente.setCpf(12312312312L);
+		cliente.setNome("Rodrigo");
+		cliente.setCidade("São Paulo");
+		cliente.setEnd("End");
+		cliente.setEstado("SP");
+		cliente.setNumero(10);
+		cliente.setTel(1199999999L);
+		clienteDao.cadastrar(cliente);
+	}
+	
+	@Test
+	public void pesquisarCliente() {
+		Cliente clienteConsultado = clienteDao.consultar(cliente.getCpf());
+		Assert.assertNotNull(clienteConsultado);
+	}
+	
+	@Test
+	public void salvarCliente() throws TipoChaveNaoEncontradaException {
+		cliente.setCpf(56565656565L);
+		Boolean retorno = clienteDao.cadastrar(cliente);
+		Assert.assertTrue(retorno);
+	}
+	
+	
+	@Test
+	public void excluirCliente() {
+		clienteDao.excluir(cliente.getCpf());
+	}
+	
+	@Test
+	public void alterarCliente() throws TipoChaveNaoEncontradaException {
+		cliente.setNome("Rodrigo Pires");
+		clienteDao.alterar(cliente);
+		Assert.assertEquals("Rodrigo Pires", cliente.getNome());
+	}
+	
+	@Test
+	public void buscarTodos() {
+		Collection<Cliente> list = clienteDao.buscarTodos();
+		assertTrue(list != null);
+		assertTrue(list.size() == 2);
+	}
+}
