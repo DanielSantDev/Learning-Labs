58a68e798d2654ceca83dbe94f3fd1eaea45e84b
Daniel <daniel10gja@gmail.com>
2024-06-11 20:34:16 -0300

    Create ProdutoDAOTest.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-25/src/test/java/br/com/danielsantdev/ProdutoDAOTest.java
@@ -0,0 +1,69 @@
+package test.java.br.com.danielsantdev;
+
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import test.java.br.com.danielsantdev.dao.IProdutoDAO;
+import test.java.br.com.danielsantdev.dao.ProdutoDAO;
+import test.java.br.com.danielsantdev.domain.Produto;
+import test.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+
+public class ProdutoDAOTest {
+	
+	private IProdutoDAO produtoDao;
+
+	private Produto produto;
+	
+	public ProdutoDAOTest() {
+		produtoDao = new ProdutoDAO();
+	}
+	
+	@Before
+	public void init() throws TipoChaveNaoEncontradaException {
+		produto = new Produto();
+		produto.setCodigo("A1");
+		produto.setDescricao("Produto 1");
+		produto.setNome("Produto 1");
+		produto.setValor(BigDecimal.TEN);
+		produtoDao.cadastrar(produto);
+	}
+	
+	@Test
+	public void pesquisar() {
+		Produto produto = this.produtoDao.consultar(this.produto.getCodigo());
+		Assert.assertNotNull(produto);
+	}
+	
+	@Test
+	public void salvar() throws TipoChaveNaoEncontradaException {
+		produto.setCodigo("A2");
+		Boolean retorno = produtoDao.cadastrar(produto);
+		Assert.assertTrue(retorno);
+	}
+	
+	@Test
+	public void excluir() {
+		produtoDao.excluir(produto.getCodigo());
+	}
+	
+	@Test
+	public void alterarCliente() throws TipoChaveNaoEncontradaException {
+		produto.setNome("Rodrigo Pires");
+		produtoDao.alterar(produto);
+		
+		Assert.assertEquals("Rodrigo Pires", produto.getNome());
+	}
+	
+	@Test
+	public void buscarTodos() {
+		Collection<Produto> list = produtoDao.buscarTodos();
+		assertTrue(list != null);
+		assertTrue(list.size() == 2);
+	}
+}
