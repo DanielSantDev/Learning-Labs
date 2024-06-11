7d58c7d700d35b44e7d593ff06b72f63112699bb
Daniel <daniel10gja@gmail.com>
2024-06-10 23:11:20 -0300

    Create Pessoa.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-23/src/danielsantdev/Pessoa.java
@@ -0,0 +1,24 @@
+package danielsantdev;
+
+public class Pessoa {
+    private String nome;
+    private String sexo;
+
+    public Pessoa(String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{nome='" + nome + "', sexo='" + sexo + "'}";
+    }
+}
