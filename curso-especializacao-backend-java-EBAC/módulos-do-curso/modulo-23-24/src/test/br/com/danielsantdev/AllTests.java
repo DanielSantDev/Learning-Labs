2fef38fc1cfde4875148c728a58172c35d87e394
Daniel <daniel10gja@gmail.com>
2024-06-11 22:38:33 -0300

    Create AllTests.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-23-24/src/test/br/com/danielsantdev/AllTests.java
@@ -0,0 +1,9 @@
+package test.br.com.danielsantdev;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteServiceTest.class, ContratoServiceTest.class })
+public class AllTests {
+}
