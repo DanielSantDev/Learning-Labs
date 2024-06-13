a726d8dbf89c04f20a0a9e0d5bc767e7964b32fb
Daniel <daniel10gja@gmail.com>
2024-06-12 21:18:13 -0300

    Create ClienteDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/main/java/br/com/danielsantdev/dao/ClienteDAO.java
@@ -0,0 +1,29 @@
+package main.java.br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.GenericDAO;
+import br.com.danielsantdev.domain.Cliente;
+
+public class ClienteDAO extends GenericDAO<Cliente, Long> implements IClienteDAO {
+
+	public ClienteDAO() {
+		super();
+	}
+
+	@Override
+	public Class<Cliente> getTipoClasse() {
+		return Cliente.class;
+	}
+
+	@Override
+	public void atualiarDados(Cliente entity, Cliente entityCadastrado) {
+		entityCadastrado.setCidade(entity.getCidade());
+		entityCadastrado.setCpf(entity.getCpf());
+		entityCadastrado.setEnd(entity.getEnd());
+		entityCadastrado.setEstado(entity.getEstado());
+		entityCadastrado.setNome(entity.getNome());
+		entityCadastrado.setNumero(entity.getNumero());
+		entityCadastrado.setTel(entity.getTel());
+		
+	}
+
+}
