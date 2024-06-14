e41b8cd5b472f2557d6cdf9c875e73fff69146b5
Daniel <daniel10gja@gmail.com>
2024-06-13 21:51:44 -0300

    Create IProdutoDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/main/java/br/com/danielsantdev/dao/IProdutoDAO.java
@@ -0,0 +1,9 @@
+package br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.IGenericDAO;
+import br.com.danielsantdev.domain.Produto;
+
+
+public interface IProdutoDAO extends IGenericDAO<Produto, String>{
+
+}
