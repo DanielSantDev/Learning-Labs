4880a445bbe1991efb64f5b869a51d03028e836a
Daniel <daniel10gja@gmail.com>
2024-06-14 18:42:49 -0300

    Create ProdutoDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/main/java/br/com/danielsantdev/dao/ProdutoDAO.java
@@ -0,0 +1,25 @@
+package br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.GenericDAO;
+import br.com.danielsantdev.domain.Produto;
+
+public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoDAO {
+	
+	public ProdutoDAO() {
+		super();
+	}
+
+	@Override
+	public Class<Produto> getTipoClasse() {
+		return Produto.class;
+	}
+
+	@Override
+	public void atualiarDados(Produto entity, Produto entityCadastrado) {
+		entityCadastrado.setCodigo(entity.getCodigo());
+		entityCadastrado.setDescricao(entity.getDescricao());
+		entityCadastrado.setNome(entity.getNome());
+		entityCadastrado.setValor(entity.getValor());
+	}
+
+}
