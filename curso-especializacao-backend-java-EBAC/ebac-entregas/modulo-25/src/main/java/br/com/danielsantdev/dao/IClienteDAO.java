a2f30178069bca1ffc536c3d13cd85b542de66de
Daniel <daniel10gja@gmail.com>
2024-06-13 21:51:12 -0300

    Create IClienteDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/main/java/br/com/danielsantdev/dao/IClienteDAO.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.IGenericDAO;
+import br.com.danielsantdev.domain.Cliente;
+
+
+public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
+
+
+}
