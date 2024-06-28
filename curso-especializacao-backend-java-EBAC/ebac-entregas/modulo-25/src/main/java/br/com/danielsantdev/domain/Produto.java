8123e6ac774a6487755da2685f99bbbb989997e0
Daniel <daniel10gja@gmail.com>
2024-06-27 22:08:37 -0300

    Create Produto.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/main/java/br/com/danielsantdev/domain/Produto.java
@@ -0,0 +1,51 @@
+package main.java.br.com.danielsantdev.domain;
+
+import java.math.BigDecimal;
+
+import main.java.br.com.danielsantdev.anotacao.TipoChave;
+import main.java.br.com.danielsantdev.dao.Persistente;
+
+public class Produto implements Persistente {
+
+	@TipoChave("getCodigo")
+	private String codigo;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private BigDecimal valor;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+}
