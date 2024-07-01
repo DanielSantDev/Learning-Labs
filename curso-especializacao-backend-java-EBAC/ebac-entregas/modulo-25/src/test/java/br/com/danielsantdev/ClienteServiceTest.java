2b2889658c621e18c30ff8ecfb0bdc0f61de3659
Daniel <daniel10gja@gmail.com>
2024-07-01 16:45:48 -0300

    Create ClienteServiceTest.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-25/src/test/java/br/com/danielsantdev/ClienteServiceTest.java
@@ -0,0 +1,63 @@
+package test.java.br.com.danielsantdev;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import test.java.br.com.danielsantdev.dao.ClienteDaoMock;
+import main.java.br.com.danielsantdev.dao.IClienteDAO;
+import main.java.br.com.danielsantdev.domain.Cliente;
+import main.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+import main.java.br.com.danielsantdev.services.ClienteService;
+import main.java.br.com.danielsantdev.services.IClienteService;
+
+public class ClienteServiceTest {
+	
+	private IClienteService clienteService;
+	
+	private Cliente cliente;
+	
+	public ClienteServiceTest() {
+		IClienteDAO dao = new ClienteDaoMock();
+		clienteService = new ClienteService(dao);
+	}
+	
+	@Before
+	public void init() {
+		cliente = new Cliente();
+		cliente.setCpf(12312312312L);
+		cliente.setNome("Rodrigo");
+		cliente.setCidade("São Paulo");
+		cliente.setEnd("End");
+		cliente.setEstado("SP");
+		cliente.setNumero(10);
+		cliente.setTel(1199999999L);
+		
+	}
+	
+	@Test
+	public void pesquisarCliente() {
+		Cliente clienteConsultado = clienteService.buscarPorCPF(cliente.getCpf());
+		Assert.assertNotNull(clienteConsultado);
+	}
+	
+	@Test
+	public void salvarCliente() throws TipoChaveNaoEncontradaException {
+		Boolean retorno = clienteService.cadastrar(cliente);
+		
+		Assert.assertTrue(retorno);
+	}
+	
+	@Test
+	public void excluirCliente() {
+		clienteService.excluir(cliente.getCpf());
+	}
+	
+	@Test
+	public void alterarCliente() throws TipoChaveNaoEncontradaException {
+		cliente.setNome("Rodrigo Pires");
+		clienteService.alterar(cliente);
+		
+		Assert.assertEquals("Rodrigo Pires", cliente.getNome());
+	}
+}
