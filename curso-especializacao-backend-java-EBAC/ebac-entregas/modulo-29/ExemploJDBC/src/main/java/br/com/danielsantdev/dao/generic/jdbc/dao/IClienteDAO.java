9e8322543c159d5d889a26f0424507839d85db92
Daniel <daniel10gja@gmail.com>
2024-07-03 23:49:29 -0300

    Create IClienteDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package main.java.br.com.danielsantdev.dao.generic.jdbc.dao;
+
+import java.util.List;
+
+import main.java.br.com.danielsantdev.domin.Cliente;
+
+public interface IClienteDAO {
+
+	public Integer cadastrar(Cliente cliente) throws Exception;
+	
+	public Integer atualizar(Cliente cliente) throws Exception;
+	
+	public Cliente buscar(String codigo) throws Exception;
+	
+	public List<Cliente> buscarTodos() throws Exception;
+	
+	public Integer excluir(Cliente cliente) throws Exception;
+}
