d8d38e03ccd18bd02f1abe20af54706477be7276
Daniel <daniel10gja@gmail.com>
2024-07-15 20:50:55 -0300

    Update Main.java

--- ExerciciosJava/150-BasicExercises/src/Main.java
+++ ExerciciosJava/150-BasicExercises/src/Main.java
@@ -1,6 +1,140 @@
+import javax.lang.model.element.VariableElement;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Daniel Santana");
+//        //1
+//        System.out.println("Hello");
+//        System.out.println("Daniel Santana\n");
+//
+//        //2
+//        System.out.println(74 + 36);
+//
+//        //3
+//        System.out.println(50 / 3);
+//
+//        //4
+//        int a = (-5 + 8 * 6);
+//        float b = ((55 + 9) % 9);
+//        float c = (20+ -3 * 5 / 8);
+//        float d = (5 + 15 / 3 * 2 -8 % 3);
+//        System.out.println("\nA: " + a + "\nB: " + b + "\nC: " + c + "\nD: " + d);
+//
+//        //5
+        Scanner entrada = new Scanner(System.in);
+//        float p = entrada.nextFloat();
+//        float s = entrada.nextFloat();
+//        System.out.println("Produto: " + p * s);
+//
+//        //6
+//        float num1 = entrada.nextFloat();
+//        float num2 = entrada.nextFloat();
+//        System.out.println("\nOs dois numeros sao:" + num1 + " e " + num2);
+//        System.out.println("Adicao: " + (num1 + num2));
+//        System.out.println("Subtracao: " + (num1 - num2));
+//        System.out.println("Multiplicação: " + num1 * num2);
+//        System.out.println("Divisao: " + num1 / num2);
+//        System.out.println("Resto: " + num1 % num2);
+//
+//        //7
+//        int numero = entrada.nextInt();
+//        for(int i=1 ; i <= 10 ; i++){
+//            System.out.println(numero + " * " + i + " = " + (numero * i));
+//        }
+
+//        //8
+//        System.out.println("   J    a   v     v  a    ");
+//        System.out.println("   J   a a   v   v  a a   ");
+//        System.out.println("J  J  aaaaa   V V  aaaaa  ");
+//        System.out.println(" JJ  a     a   V  a     a ");
+//
+//        //9
+//        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+//
+//        //10
+//        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+//
+//        //11
+//        final float PI = 3.1416f;
+//        float raio = entrada.nextFloat();
+//        System.out.println("O perimetro do circulo: " + ((PI * 2) * raio));
+//        System.out.println("A area do circulo: " + (PI * (raio*raio)));
+
+//        //12
+//        int n1 = entrada.nextInt();
+//        int n2 = entrada.nextInt();
+//        int n3 = entrada.nextInt();
+//        System.out.println("Media = " + ((n1 + n2 + n3) / 3) );
+
+        //13
+//        float base = entrada.nextFloat();
+//        float altura = entrada.nextFloat();
+//        System.out.println("O perimetro do retangulo: " + (2 * (base + altura)) );
+//        System.out.println("A area do retangulo: " + (base * altura) );
+
+//        //14
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+
+        //15
+//        int var1 = 10;
+//        int var2 = 20;
+//        System.out.println("Antes.\nVariavel 1: " + var1 + " Variavel 2: " + var2);
+//        var1 = var1 + var2;
+//        var2 = var1 - var2;
+//        var1 = var1 - var2;
+//        System.out.println("\nDepois.\nVariavel 1: " + var1 + " Variavel 2: " + var2);
+
+//        //16
+//        System.out.println(" +\"\"\"\"\"+ ");
+//        System.out.println("[| o o |]");
+//        System.out.println(" |  ^  | ");
+//        System.out.println(" | '-' | ");
+//        System.out.println(" +-----+");
+
+//        //17
+//        long binario1, binario2;
+//        int i = 0, resto = 0;
+//
+//        int[] sum = new int[20];
+//        binario1 = entrada.nextLong();
+//        binario2 = entrada.nextLong();
+//
+//        while (binario1 != 0 || binario2 != 0)
+//        {
+//            // Calculate the sum of binary digits and update the remainder
+//            sum[i++] = (int)((binario1 % 10 + binario2 % 10 + resto) % 2);
+//            resto = (int)((binario1 % 10 + binario2 % 10 + resto) / 2);
+//            binario1 = binario1 / 10;
+//            binario2 = binario2 / 10;
+//        }
+//
+//        if (resto != 0) {
+//            sum[i++] = resto;
+//        }
+//
+//        i--;
+//
+//        System.out.println("Soma dos numeros binarios: ");
+//        while (i >= 0) {
+//            System.out.print(sum[i--]);
+//        }
+
+        //18
+        
     }
+
 }
