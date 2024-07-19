10cfe3a6ba41d827c4d904a02a105f2b4292e7df
Daniel <daniel10gja@gmail.com>
2024-07-18 23:50:03 -0300

    Update part2.java

--- ExerciciosJava/150-BasicExercises/src/part2.java
+++ ExerciciosJava/150-BasicExercises/src/part2.java
@@ -70,7 +70,13 @@ public class part2 {
 //        System.out.println("Area do Poligono: " + area);
 
         //36
-        
+        double latitude1 = entrada.nextDouble();
+        double longitude1 = entrada.nextDouble();
+
+        double latitude2 = entrada.nextDouble();
+        double longitude2 = entrada.nextDouble();
+
+        System.out.println("A distancia entre os pontos é: " + distance_Between_LatLong(latitude1, longitude1, latitude2, longitude2));
     }
 
     public static int sumDigits(long n) {
@@ -83,4 +89,16 @@ public class part2 {
 
         return sum;
     }
+
+    public static double distance_Between_LatLong(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371.01;
+
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+
 }
