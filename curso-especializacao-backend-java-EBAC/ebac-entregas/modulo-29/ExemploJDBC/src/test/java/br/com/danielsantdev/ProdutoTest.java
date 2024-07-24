baf9af6e71b0749f1b58b3aa3ef4ba8d2ccd34a4
Daniel <daniel10gja@gmail.com>
2024-07-24 00:05:55 -0300

    Create ProdutoTest.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/test/java/br/com/danielsantdev/ProdutoTest.java
@@ -0,0 +1,11 @@
+package test.java.br.com.danielsantdev;
+
+import main.java.br.com.danielsantdev.dao.generic.jdbc.dao.IProdutoDAO;
+import main.java.br.com.danielsantdev.dao.generic.jdbc.dao.ProdutoDAO;
+
+
+public class ProdutoTest {
+
+    private IProdutoDAO produtoDAO;
+
+}
