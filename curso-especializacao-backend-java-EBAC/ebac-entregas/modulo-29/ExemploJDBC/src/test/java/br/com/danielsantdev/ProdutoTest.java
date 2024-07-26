2df3f83c3934444c270be8e858ad3db42d72d4db
Daniel <daniel10gja@gmail.com>
2024-07-26 00:20:00 -0300

    Update ProdutoTest.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/test/java/br/com/danielsantdev/ProdutoTest.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/test/java/br/com/danielsantdev/ProdutoTest.java
@@ -1,11 +1,143 @@
 package test.java.br.com.danielsantdev;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
 import main.java.br.com.danielsantdev.dao.generic.jdbc.dao.IProdutoDAO;
 import main.java.br.com.danielsantdev.dao.generic.jdbc.dao.ProdutoDAO;
-
+import main.java.br.com.danielsantdev.domin.Produto;
 
 public class ProdutoTest {
 
     private IProdutoDAO produtoDAO;
+    
+    @Test
+    public void cadastrarTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
 
+        Produto produto = new Produto();
+        produto.setCodigo("10");
+        produto.setNome("Placa de Video GTX 1660 Super");
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertTrue(countCad == 1);
+
+        Produto produtoBD = produtoDAO.buscar("10");
+        assertNotNull(produtoBD);
+        assertEquals(produto.getCodigo(), produtoBD.getCodigo());
+        assertEquals(produto.getNome(), produtoBD.getNome());
+
+        Integer countDel = produtoDAO.excluir(produtoBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void buscarTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setCodigo("10");
+        produto.setNome("Placa de Video GTX 1660 Super");
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertTrue(countCad == 1);
+
+        Produto produtoBD = produtoDAO.buscar("10");
+        assertNotNull(produtoBD);
+        assertEquals(produto.getCodigo(), produtoBD.getCodigo());
+        assertEquals(produto.getNome(), produtoBD.getNome());
+
+        Integer countDel = produtoDAO.excluir(produtoBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void excluirTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setCodigo("10");
+        produto.setNome("Placa de Video GTX 1660 Super");
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertTrue(countCad == 1);
+
+        Produto produtoBD = produtoDAO.buscar("10");
+        assertNotNull(produtoBD);
+        assertEquals(produto.getCodigo(), produtoBD.getCodigo());
+        assertEquals(produto.getNome(), produtoBD.getNome());
+
+        Integer countDel = produtoDAO.excluir(produtoBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void buscarTodosTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setCodigo("10");
+        produto.setNome("Placa de Video GTX 1660 Super");
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertTrue(countCad == 1);
+
+        Produto produtos = new Produto();
+        produtos.setCodigo("20");
+        produtos.setNome("Processador I7 7500 3.8GHZ");
+        Integer countCad2 = produtoDAO.cadastrar(produtos);
+        assertTrue(countCad2 == 1);
+
+        List<Produto> list = produtoDAO.buscarTodos();
+        assertNotNull(list);
+        assertEquals(2, list.size());
+
+        int countDel = 0;
+        for (Produto cli : list) {
+            produtoDAO.excluir(cli);
+            countDel++;
+        }
+        assertEquals(list.size(), countDel);
+
+        list = produtoDAO.buscarTodos();
+        assertEquals(list.size(), 0);
+
+    }
+
+    @Test
+    public void atualizarTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setCodigo("10");
+        produto.setNome("Placa de Video GTX 1660 Super");
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertTrue(countCad == 1);
+
+        Produto produtoBD = produtoDAO.buscar("10");
+        assertNotNull(produtoBD);
+        assertEquals(produto.getCodigo(), produtoBD.getCodigo());
+        assertEquals(produto.getNome(), produtoBD.getNome());
+
+        produtoBD.setCodigo("20");
+        produtoBD.setNome("Processador I7 7500 3.8GHZ");
+        Integer countUpdate = produtoDAO.atualizar(produtoBD);
+        assertTrue(countUpdate == 1);
+
+        Produto produtoBD1 = produtoDAO.buscar("10");
+        assertNull(produtoBD1);
+
+        Produto produtoBD2 = produtoDAO.buscar("20");
+        assertNotNull(produtoBD2);
+        assertEquals(produtoBD.getId(), produtoBD2.getId());
+        assertEquals(produtoBD.getCodigo(), produtoBD2.getCodigo());
+        assertEquals(produtoBD.getNome(), produtoBD2.getNome());
+
+        List<Produto> list = produtoDAO.buscarTodos();
+        for (Produto cli : list) {
+            produtoDAO.excluir(cli);
+        }
+    }
 }
