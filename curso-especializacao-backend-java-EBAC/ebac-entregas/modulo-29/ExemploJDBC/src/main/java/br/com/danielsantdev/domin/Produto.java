d893eadeb2e36a635486f57824e9ab63bc3a7781
Daniel <daniel10gja@gmail.com>
2024-07-25 23:48:38 -0300

    Update Produto.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/domin/Produto.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/domin/Produto.java
@@ -1,4 +1,31 @@
 package main.java.br.com.danielsantdev.domin;
 
 public class Produto {
+
+    private Long id;
+    private  String codigo, nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
