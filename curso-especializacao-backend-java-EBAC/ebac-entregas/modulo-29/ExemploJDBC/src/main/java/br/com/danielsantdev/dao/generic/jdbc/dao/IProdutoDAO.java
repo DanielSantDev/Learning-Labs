703e2687f86f1ddebfcb801c46f080f78181012d
Daniel <daniel10gja@gmail.com>
2024-07-25 23:48:25 -0300

    Update IProdutoDAO.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/dao/IProdutoDAO.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/dao/IProdutoDAO.java
@@ -1,7 +1,18 @@
 package main.java.br.com.danielsantdev.dao.generic.jdbc.dao;
 
+import main.java.br.com.danielsantdev.domin.Produto;
+import java.util.List;
+
 public interface IProdutoDAO {
 
+    public Integer cadastrar(Produto produto) throws Exception;
 
+    public Integer atualizar(Produto produto) throws Exception;
+
+    public Produto buscar(String codigo) throws Exception;
+
+    public List<Produto> buscarTodos() throws Exception;
+
+    public Integer excluir(Produto produto) throws Exception;
 
 }
