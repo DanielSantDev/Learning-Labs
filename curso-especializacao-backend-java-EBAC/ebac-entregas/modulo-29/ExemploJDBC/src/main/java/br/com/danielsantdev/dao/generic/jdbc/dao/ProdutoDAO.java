2c823306f4995663d910f03b86f12b7ab7efb749
Daniel <daniel10gja@gmail.com>
2024-07-26 00:18:38 -0300

    Update ProdutoDAO.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/dao/ProdutoDAO.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/dao/ProdutoDAO.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-import main.java.br.com.danielsantdev.domin.Cliente;
 import main.java.br.com.danielsantdev.domin.Produto;
 import main.java.br.com.danielsantdev.dao.generic.jdbc.ConnectionFactory;
 
@@ -79,54 +79,92 @@ public class ProdutoDAO implements IProdutoDAO{
 
     @Override
     public List<Produto> buscarTodos() throws Exception {
-        return null;
+        Connection connection = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<Produto> list = new ArrayList<>();
+        Produto produto = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = getSqlSelectAll();
+            stm = connection.prepareStatement(sql);
+            rs = stm.executeQuery();
+
+            while (rs.next()) {
+                produto = new Produto();
+                Long id = rs.getLong("ID");
+                String nome = rs.getString("NOME");
+                String cd = rs.getString("CODIGO");
+                produto.setId(id);
+                produto.setNome(nome);
+                produto.setCodigo(cd);
+                list.add(produto);
+            }
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            closeConnection(connection, stm, rs);
+        }
+        return list;
     }
 
     @Override
     public Integer excluir(Produto produto) throws Exception {
-        return null;
+        Connection connection = null;
+        PreparedStatement stm = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = getSqlDelete();
+            stm = connection.prepareStatement(sql);
+            adicionarParametrosDelete(stm, produto);
+            return stm.executeUpdate();
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            closeConnection(connection, stm, null);
+        }
     }
 
     private String getSqlInsert() {
         StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO TB_CLIENTE (ID, CODIGO, NOME) ");
-        sb.append("VALUES (nextval('SQL_CLIENTE'),?,?)");
+        sb.append("INSERT INTO TB_PRODUTO (ID, CODIGO, NOME) ");
+        sb.append("VALUES (nextval('SQL_PRODUTO'),?,?)");
         return sb.toString();
     }
 
-    private void adicionarParametrosInsert(PreparedStatement stm, Produto cliente) throws SQLException {
-        stm.setString(1, cliente.getCodigo());
-        stm.setString(2, cliente.getNome());
+    private void adicionarParametrosInsert(PreparedStatement stm, Produto produto) throws SQLException {
+        stm.setString(1, produto.getCodigo());
+        stm.setString(2, produto.getNome());
     }
 
     private String getSqlUpdate() {
         StringBuilder sb = new StringBuilder();
-        sb.append("UPDATE TB_CLIENTE ");
+        sb.append("UPDATE TB_PRODUTO ");
         sb.append("SET NOME = ?, CODIGO = ? ");
         sb.append("WHERE ID = ?");
         return sb.toString();
     }
 
-    private void adicionarParametrosUpdate(PreparedStatement stm, Produto cliente) throws SQLException {
-        stm.setString(1, cliente.getNome());
-        stm.setString(2, cliente.getCodigo());
-        stm.setLong(3, cliente.getId());
+    private void adicionarParametrosUpdate(PreparedStatement stm, Produto produto) throws SQLException {
+        stm.setString(1, produto.getNome());
+        stm.setString(2, produto.getCodigo());
+        stm.setLong(3, produto.getId());
     }
 
     private String getSqlDelete() {
         StringBuilder sb = new StringBuilder();
-        sb.append("DELETE FROM TB_CLIENTE ");
+        sb.append("DELETE FROM TB_PRODUTO ");
         sb.append("WHERE CODIGO = ?");
         return sb.toString();
     }
 
-    private void adicionarParametrosDelete(PreparedStatement stm, Cliente cliente) throws SQLException {
-        stm.setString(1, cliente.getCodigo());
+    private void adicionarParametrosDelete(PreparedStatement stm, Produto produto) throws SQLException {
+        stm.setString(1, produto.getCodigo());
     }
 
     private String getSqlSelect() {
         StringBuilder sb = new StringBuilder();
-        sb.append("SELECT * FROM TB_CLIENTE ");
+        sb.append("SELECT * FROM TB_PRODUTO ");
         sb.append("WHERE CODIGO = ?");
         return sb.toString();
     }
@@ -137,7 +175,7 @@ public class ProdutoDAO implements IProdutoDAO{
 
     private String getSqlSelectAll() {
         StringBuilder sb = new StringBuilder();
-        sb.append("SELECT * FROM TB_CLIENTE");
+        sb.append("SELECT * FROM TB_PRODUTO");
         return sb.toString();
     }
 
@@ -157,5 +195,4 @@ public class ProdutoDAO implements IProdutoDAO{
             e1.printStackTrace();
         }
     }
-
 }
