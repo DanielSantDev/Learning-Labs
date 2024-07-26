b081b27e33b5a5b782969799b4ce538741f308c2
Daniel <daniel10gja@gmail.com>
2024-07-26 00:18:30 -0300

    Update ConnectionFactory.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/ConnectionFactory.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/ExemploJDBC/src/main/java/br/com/danielsantdev/dao/generic/jdbc/ConnectionFactory.java
@@ -24,7 +24,7 @@ public class ConnectionFactory {
 	private static Connection initConnection() {
         try {
             return DriverManager.getConnection(
-            		"jdbc:postgresql://localhost:5432/clientes", "postgres", "475962");
+            		"jdbc:postgresql://localhost:5432/produtos", "postgres", "475962");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
