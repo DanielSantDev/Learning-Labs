83fe932fadf4cdd7e7668a6469346cd1a5366201
Daniel <daniel10gja@gmail.com>
2024-07-26 23:28:29 -0300

    Update part2.java

--- ExerciciosJava/150-BasicExercises/src/part2.java
+++ ExerciciosJava/150-BasicExercises/src/part2.java
@@ -85,9 +85,28 @@ public class part2 {
 //            System.out.println(texto[i]);
 //        }
 
-        //38
-        String test = "Daniel San 123456 d a ¨7&)(*";
-        count(test);
+//        //38
+//        String test = "Daniel San 123456 d a ¨7&)(*";
+//        count(test);
+
+        //39
+        int amount = 0; // Initialize a counter for three-digit numbers
+
+        // Iterate through three nested loops to generate all unique three-digit numbers
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                for (int k = 1; k <= 4; k++) {
+                    // Check if 'i,' 'j,' and 'k' are all different (not equal)
+                    if (k != i && k != j && i != j) {
+                        amount++; // Increment the counter
+                        System.out.println(i + "" + j + "" + k); // Print the generated three-digit number
+                    }
+                }
+            }
+        }
+
+        // Display the total number of generated three-digit numbers
+        System.out.println("Total number of the three-digit-number is " + amount);
 
     }
 
