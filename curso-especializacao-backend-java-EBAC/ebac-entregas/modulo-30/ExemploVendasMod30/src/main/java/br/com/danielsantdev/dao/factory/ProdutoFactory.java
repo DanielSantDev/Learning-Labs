06e4aaf0afb8c926e70dc1fd9c851df0e90fba3b
Daniel <daniel10gja@gmail.com>
2024-08-01 00:41:25 -0300

    Create ProdutoFactory.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/dao/factory/ProdutoFactory.java
@@ -0,0 +1,20 @@
+package main.java.br.com.danielsantdev.dao.factory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import main.java.br.com.danielsantdev.domain.Produto;
+
+public class ProdutoFactory {
+
+	
+	public static Produto convert(ResultSet rs) throws SQLException {
+		Produto prod = new Produto();
+		prod.setId(rs.getLong("ID_PRODUTO"));
+		prod.setCodigo(rs.getString("CODIGO"));
+		prod.setNome(rs.getString("NOME"));
+		prod.setDescricao(rs.getString("DESCRICAO"));
+		prod.setValor(rs.getBigDecimal("VALOR"));
+		return prod;
+	}
+}
