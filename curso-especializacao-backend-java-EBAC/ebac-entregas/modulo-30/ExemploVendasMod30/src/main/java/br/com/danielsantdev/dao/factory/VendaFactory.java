96355e8a88b238dd5c3e08c7c03fc6fe7f050717
Daniel <daniel10gja@gmail.com>
2024-08-01 00:41:33 -0300

    Create VendaFactory.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/dao/factory/VendaFactory.java
@@ -0,0 +1,23 @@
+package main.java.br.com.danielsantdev.dao.factory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import main.java.br.com.danielsantdev.domain.Venda;
+import main.java.br.com.danielsantdev.domain.Cliente;
+import main.java.br.com.danielsantdev.domain.Venda.Status;
+
+public class VendaFactory {
+
+	public static Venda convert(ResultSet rs) throws SQLException {
+		Cliente cliente = ClienteFactory.convert(rs);
+		Venda venda = new Venda();
+		venda.setCliente(cliente);
+		venda.setId(rs.getLong("ID_VENDA"));
+		venda.setCodigo(rs.getString("CODIGO"));
+		venda.setValorTotal(rs.getBigDecimal("VALOR_TOTAL"));
+		venda.setDataVenda(rs.getTimestamp("DATA_VENDA").toInstant());
+		venda.setStatus(Status.getByName(rs.getString("STATUS_VENDA")));
+		return venda;
+	}
+}
