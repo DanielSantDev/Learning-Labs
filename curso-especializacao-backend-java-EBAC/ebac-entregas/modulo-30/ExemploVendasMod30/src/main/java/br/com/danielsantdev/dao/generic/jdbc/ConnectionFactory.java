2fadbfb0ff24d8b909fb31e82177c4056f0710f3
Daniel <daniel10gja@gmail.com>
2024-08-01 22:56:20 -0300

    Create ConnectionFactory.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/dao/generic/jdbc/ConnectionFactory.java
@@ -0,0 +1,36 @@
+package main.java.br.com.danielsantdev.dao.generic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	private static Connection connection;
+	
+	private ConnectionFactory(Connection connection) {
+		
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		if (connection == null) {
+			connection = initConnection();
+			return connection;
+		} else if (connection.isClosed()) {
+			connection = initConnection();
+			return connection;
+		} else {
+			return connection;
+		}
+	}
+	
+	private static Connection initConnection() {
+        try {
+            return DriverManager.getConnection(
+            		"jdbc:postgresql://localhost:5432/vendas_online", "postgres", "475962");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
+}
