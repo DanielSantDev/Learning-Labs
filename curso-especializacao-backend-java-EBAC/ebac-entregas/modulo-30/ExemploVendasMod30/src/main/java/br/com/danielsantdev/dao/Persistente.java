812bdaf6eadf2f96e43e1149a8aa3df3940abbd1
Daniel <daniel10gja@gmail.com>
2024-08-01 23:12:38 -0300

    Create Persistente.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/dao/Persistente.java
@@ -0,0 +1,15 @@
+package main.java.br.com.danielsantdev.dao;
+
+/**
+ * Classe que representa todas as entidades ou objetos da aplicação que seram salvas no banco de dados
+ *
+ */
+
+public interface Persistente {
+
+    //public Long getCodigo();
+	
+	public Long getId();
+	
+	public void setId(Long id);
+}
