bb978559bb64dabf0e133ef2c726b376c98faa09
Daniel <daniel10gja@gmail.com>
2024-08-02 00:04:07 -0300

    Create ProdutoDaoMock.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/test/java/br/com/danielsantdev/dao/ProdutoDaoMock.java
@@ -0,0 +1,42 @@
+package test.java.br.com.danielsantdev.dao;
+
+import java.util.Collection;
+
+import main.java.br.com.danielsantdev.dao.IProdutoDAO;
+import main.java.br.com.danielsantdev.domain.Produto;
+import main.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+
+public class ProdutoDaoMock implements IProdutoDAO {
+
+	@Override
+	public Boolean cadastrar(Produto entity) throws TipoChaveNaoEncontradaException {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void excluir(String valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alterar(Produto entity) throws TipoChaveNaoEncontradaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Produto consultar(String valor) {
+		Produto produto = new Produto();
+		produto.setCodigo(valor);
+		return produto;
+	}
+
+	@Override
+	public Collection<Produto> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
