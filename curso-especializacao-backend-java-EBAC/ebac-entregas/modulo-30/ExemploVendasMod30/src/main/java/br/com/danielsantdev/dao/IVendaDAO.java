8b3f2971de410f526f9f47234ce2a1ba3537aee5
Daniel <daniel10gja@gmail.com>
2024-08-01 23:12:34 -0300

    Create IVendaDAO.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/dao/IVendaDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package main.java.br.com.danielsantdev.dao;
+
+import main.java.br.com.danielsantdev.dao.generic.IGenericDAO;
+import main.java.br.com.danielsantdev.domain.Venda;
+import main.java.br.com.danielsantdev.exceptions.DAOException;
+import main.java.br.com.danielsantdev.exceptions.TipoChaveNaoEncontradaException;
+
+public interface IVendaDAO extends IGenericDAO<Venda, String> {
+
+	public void finalizarVenda(Venda venda) throws TipoChaveNaoEncontradaException, DAOException;
+	
+	public void cancelarVenda(Venda venda) throws TipoChaveNaoEncontradaException, DAOException;
+}
