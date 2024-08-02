2dbc7de725a5add5d526654910f23c84452319f5
Daniel <daniel10gja@gmail.com>
2024-08-02 00:04:38 -0300

    Create TableException.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/exceptions/TableException.java
@@ -0,0 +1,12 @@
+package main.java.br.com.danielsantdev.exceptions;
+
+public class TableException extends Exception {
+
+	
+	private static final long serialVersionUID = -7509649433607067138L;
+
+	public TableException(String msg) {
+		super(msg);
+    }
+
+}
