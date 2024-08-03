5bd05f2a67a28f879073cebc9f1e5a1219878313
Daniel <daniel10gja@gmail.com>
2024-08-03 00:01:31 -0300

    Update Produto.java

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/domain/Produto.java
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-30/ExemploVendasMod30/src/main/java/br/com/danielsantdev/domain/Produto.java
@@ -26,6 +26,9 @@ public class Produto implements Persistente {
 	@ColunaTabela(dbName = "valor", setJavaName = "setValor")
 	private BigDecimal valor;
 
+	@ColunaTabela(dbName = "marca", setJavaName = "setMarca")
+	private String marca;
+
 	public String getCodigo() {
 		return codigo;
 	}
@@ -64,6 +67,14 @@ public class Produto implements Persistente {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
 	}
 	
 }
