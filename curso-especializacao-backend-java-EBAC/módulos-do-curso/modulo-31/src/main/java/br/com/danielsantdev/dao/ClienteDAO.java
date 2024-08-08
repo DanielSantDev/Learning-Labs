aa440e6cc7bc8b009caae02ede0a07913dc6d154
Daniel <daniel10gja@gmail.com>
2024-08-07 21:28:52 -0300

    Create ClienteDAO.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-31/src/main/java/br/com/danielsantdev/dao/ClienteDAO.java
@@ -0,0 +1,92 @@
+package main.java.br.com.danielsantdev.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import main.java.br.com.danielsantdev.domain.Cliente;
+
+public class ClienteDAO implements IClienteDAO {
+	
+	@Override
+	public Cliente cadastrar(Cliente cliente) {
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("ExemploJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+		 
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		return cliente;
+	}
+
+	@Override
+	public Cliente buscarPorID(Long id) {
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("ExemploJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		Cliente cliente = entityManager.find(Cliente.class, id);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		return cliente;
+	}
+
+	@Override
+	public void excluir(Cliente cliente) {
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("ExemploJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		cliente = entityManager.merge(cliente);
+		entityManager.remove(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+	@Override
+	public Cliente alterar(Cliente cliente) {
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("ExemploJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		cliente = entityManager.merge(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		return cliente;
+	}
+
+	@Override
+	public List<Cliente> buscarTodos() {
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("ExemploJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		List<Cliente> list = 
+				entityManager.createQuery("SELECT c FROM Cliente c", Cliente.class).getResultList();      
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		return list;
+	}
+
+}
