b6e76604e65c20d907900849258e309946172b94
Daniel <daniel10gja@gmail.com>
2024-08-12 21:52:00 -0300

    Create IComputadorDao.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-33/src/main/java/br/com/danielsantdev/dao/IComputadorDao.java
@@ -0,0 +1,8 @@
+package main.java.br.com.danielsantdev.dao;
+
+import main.java.br.com.danielsantdev.domain.Computador;
+
+public interface IComputadorDao {
+
+	public Computador cadastrar(Computador computador);
+}
