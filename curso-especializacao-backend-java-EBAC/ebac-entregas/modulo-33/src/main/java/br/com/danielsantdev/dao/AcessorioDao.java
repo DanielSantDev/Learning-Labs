9239fd8e4931a4d1a2c2cfab1a1ba5a19227a12d
Daniel <daniel10gja@gmail.com>
2024-08-14 23:11:34 -0300

    Create AcessorioDao.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-33/src/main/java/br/com/danielsantdev/dao/AcessorioDao.java
@@ -0,0 +1,28 @@
+package main.java.br.com.danielsantdev.dao;
+
+import main.java.br.com.danielsantdev.domain.Acessorio;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AcessorioDao implements IAcessorioDao {
+
+    @Override
+    public Acessorio cadastrar(Acessorio acessorio) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("ExemploJPA");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(acessorio);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+
+        return acessorio;
+    }
+
+}
