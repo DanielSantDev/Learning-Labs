445e7521e4620aa16677e0bb8d903db090ee6304
Daniel <daniel10gja@gmail.com>
2024-08-14 23:13:27 -0300

    Create Marca.java

---
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-33/src/main/java/br/com/danielsantdev/domain/Marca.java
@@ -0,0 +1,66 @@
+package main.java.br.com.danielsantdev.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_MARCA")
+public class Marca {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="marca_seq")
+    @SequenceGenerator(name="marca_seq", sequenceName="sq_marca", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    @Column(name = "CODIGO", length = 10, nullable = false, unique = true)
+    private String codigo;
+
+    @Column(name = "NOME", length = 50, nullable = false)
+    private String nome;
+
+    @Column(name = "DESCRICAO", length = 100, nullable = false)
+    private String descricao;
+
+    @OneToMany(mappedBy = "marca")
+    private List<Registro> registros;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(List<Registro> registros) {
+        this.registros = registros;
+    }
+}
