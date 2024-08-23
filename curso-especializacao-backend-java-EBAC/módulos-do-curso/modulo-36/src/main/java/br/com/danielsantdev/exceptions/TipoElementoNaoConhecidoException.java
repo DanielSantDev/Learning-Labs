b7bb372dc34001019f925b0e6d7bc421c32e294c
Daniel <daniel10gja@gmail.com>
2024-08-22 21:24:19 -0300

    Create Exceptions

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-36/src/main/java/br/com/danielsantdev/exceptions/TipoElementoNaoConhecidoException.java
@@ -0,0 +1,15 @@
+package br.com.danielsantdev.exceptions;
+
+public class TipoElementoNaoConhecidoException extends Exception {
+
+	private static final long serialVersionUID = -2268140970978666251L;
+
+	public TipoElementoNaoConhecidoException(String msg) {
+        this(msg, null);
+    }
+
+    public TipoElementoNaoConhecidoException(String msg, Throwable e) {
+        super(msg, e);
+    }
+
+}
