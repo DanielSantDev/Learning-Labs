db2b318a33977fcfb2fdbdd2133ca8ca99d86b52
Daniel <daniel10gja@gmail.com>
2024-08-22 21:24:53 -0300

    Create Test

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-36/src/test/java/br/com/danielsantdev/jpa/AllTests.java
@@ -0,0 +1,10 @@
+package br.com.danielsantdev.jpa;
+
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({ ClienteServiceTest.class, ClienteDAOTest.class,
+//	ProdutoServiceTest.class, ProdutoDAOTest.class,
+//	VendaDAOTest.class})
+
+public class AllTests {
+
+}
