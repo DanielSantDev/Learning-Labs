a7c18291bc645a34476333e5ff92d8b4d65a7b59
Daniel <daniel10gja@gmail.com>
2024-08-23 23:51:38 -0300

    Create AppTest.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-37/exemplo-gradle/app/src/test/java/br/com/danielsantdev/AppTest.java
@@ -0,0 +1,12 @@
+package test.java.br.com.danielsantdev;
+
+import main.java.br.com.danielsantdev.App;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
