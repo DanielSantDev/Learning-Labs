0c1cbdf2d309f29aea4b648edc5a061598f6e88f
Daniel <daniel10gja@gmail.com>
2024-08-24 16:52:09 -0300

    Create AppTest3.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-37/PrimeiroProjetoMavenIt/src/test/java/br/com/danielsantdev/AppTest3.java
@@ -0,0 +1,20 @@
+package test.java.br.com.danielsantdev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App2.
+ */
+public class AppTest3
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
