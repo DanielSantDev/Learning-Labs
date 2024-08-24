eca700651ef137a91ac8f056e9edeb8cccee8a1d
Daniel <daniel10gja@gmail.com>
2024-08-24 16:52:16 -0300

    Create App3.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-37/PrimeiroProjetoMavenIt/src/main/java/br/com/danielsantdev/App3.java
@@ -0,0 +1,13 @@
+package main.java.br.com.danielsantdev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
