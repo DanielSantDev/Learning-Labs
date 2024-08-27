363bef71245f74a6c8627bf694a03a42b74de870
Daniel <daniel10gja@gmail.com>
2024-08-26 23:54:58 -0300

    Create IndexController.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-38/ExemploJSF/src/main/java/br/com/danielsantdev/controller/IndexController.java
@@ -0,0 +1,17 @@
+package br.com.danielsantdev.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+
+	private static final long serialVersionUID = -3176307559028924604L;
+
+	public String goToClientPage() {
+	    return "/cliente/list.xhtml";
+	}
+}
