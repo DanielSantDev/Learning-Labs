a0408f17aa4ce3fcce2f11fe529249b902808bfa
Daniel <daniel10gja@gmail.com>
2024-09-12 23:43:36 -0300

    update

--- curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-38/ExemploJSF/src/main/java/br/com/danielsantdev/dao/IClienteDAO.java
+++
@@ -1,8 +0,0 @@
-package main.java.br.com.danielsantdev.dao;
-
-import main.java.br.com.danielsantdev.dao.generic.IGenericDAO;
-import br.com.danielsantdev.domain.Cliente;
-
-public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
-
-}
