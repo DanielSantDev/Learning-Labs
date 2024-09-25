dec161b56053fe4c99d57b8db48f43eca2bb3a2b
Daniel <daniel10gja@gmail.com>
2024-09-24 23:42:12 -0300

    Create Modulo38Application.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-38/src/main/java/br/com/danielsantdev/Modulo38Application.java
@@ -0,0 +1,13 @@
+package br.com.danielsantdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo38Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modulo38Application.class, args);
+    }
+
+}
