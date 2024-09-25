29ad9c87599976c28534930ab30a23dde1b4426e
Daniel <daniel10gja@gmail.com>
2024-09-24 23:44:15 -0300

    Create Modulo38ApplicationTests.java

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-38/src/test/java/br/com/danielsantdev/Modulo38ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielsantdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo38ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
