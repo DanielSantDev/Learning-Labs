0ae5c1ec45ba14ea551f7307e84bbfb21ad04583
Daniel <daniel10gja@gmail.com>
2024-09-24 23:38:50 -0300

    Create Controllers

---
+++ curso-especializacao-backend-java-EBAC/módulos-do-curso/modulo-38/src/main/java/br/com/danielsantdev/controller/IndexController.java
@@ -0,0 +1,25 @@
+package br.com.danielsantdev.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+
+	private static final long serialVersionUID = -354273675936287852L;
+
+	public String redirectCliente() {
+		return "/cliente/list.xhtml";
+	}
+
+	public String redirectProduto() {
+		return "/produto/list.xhtml";
+	}
+
+	public String redirectVenda() {
+		return "/venda/list.xhtml";
+	}
+}
