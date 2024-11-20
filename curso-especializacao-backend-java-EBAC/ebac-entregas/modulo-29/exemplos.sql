d925281ff33a419a2fab2bbadde13d32a4d59aa9
Daniel <daniel10gja@gmail.com>
2024-11-19 23:51:26 -0300

    Update exemplos.sql

--- curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/exemplos.sql
+++ curso-especializacao-backend-java-EBAC/ebac-entregas/modulo-29/exemplos.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tb_cliente (
 	id BIGINT,
 	nome VARCHAR(50) NOT NULL,
-    codigo VARCHAR NOT NULL,
+        codigo VARCHAR NOT NULL,
 	CONSTRAINT pk_id_cliente primary key(id)
 );
 
@@ -13,7 +13,7 @@ owned by tb_cliente.id;
 CREATE TABLE tb_produto (
 	id BIGINT,
 	nome VARCHAR(50) NOT NULL,
-    codigo VARCHAR NOT NULL,
+        codigo VARCHAR NOT NULL,
 	CONSTRAINT pk_id_produto primary key(id)
 );
 
@@ -21,3 +21,4 @@ create sequence sql_produto
 start 1
 increment 1
 owned by tb_produto.id;
+
