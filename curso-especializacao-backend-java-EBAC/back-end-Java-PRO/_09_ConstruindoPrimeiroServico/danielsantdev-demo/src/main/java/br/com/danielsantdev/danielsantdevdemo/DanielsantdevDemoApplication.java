3a4b933cb6de56cf13841838b2a9cd52efe91cd3
Daniel <daniel10gja@gmail.com>
2024-11-23 14:43:31 -0300

    Criando primeiro Servico

---
+++ curso-especializacao-backend-java-EBAC/back-end-Java-PRO/_09_ConstruindoPrimeiroServico/danielsantdev-demo/src/main/java/br/com/danielsantdev/danielsantdevdemo/DanielsantdevDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielsantdev.danielsantdevdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielsantdevDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanielsantdevDemoApplication.class, args);
+    }
+
+}
