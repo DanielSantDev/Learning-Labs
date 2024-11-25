71bcc638e622716b05cd3c0b1de2501b430b46f3
Daniel <daniel10gja@gmail.com>
2024-11-24 23:59:35 -0300

    Create data.sql

---
+++ curso-especializacao-backend-java-EBAC/back-end-Java-PRO/_09_ConstruindoPrimeiroServico/animal-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Neve', 5, 'Fiapo de manda', current_date, 'sujo', 'Daniel', 'medio');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Sky', 5, 'Caramelo', current_date, 'jovem', 'Gomes', 'Grande');
