ec499adba82eac04f63fb6113055bef70f6dd8c6
Daniel <daniel10gja@gmail.com>
2024-11-26 00:17:02 -0300

    Create AnimalServiceApplicationTests.java

---
+++ curso-especializacao-backend-java-EBAC/back-end-Java-PRO/_09_ConstruindoPrimeiroServico/animal-service/src/test/java/br/com/danielsantdev/animalservice/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielsantdev.animalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
