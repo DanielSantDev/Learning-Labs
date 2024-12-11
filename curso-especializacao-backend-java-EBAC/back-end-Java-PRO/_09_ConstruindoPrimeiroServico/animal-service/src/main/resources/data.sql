6df13f2d50f0538d958d2511ed88d4a8524119d5
Daniel <daniel10gja@gmail.com>
2024-12-10 22:36:24 -0300

    Update data.sql

--- curso-especializacao-backend-java-EBAC/back-end-Java-PRO/_09_ConstruindoPrimeiroServico/animal-service/src/main/resources/data.sql
+++ curso-especializacao-backend-java-EBAC/back-end-Java-PRO/_09_ConstruindoPrimeiroServico/animal-service/src/main/resources/data.sql
@@ -3,3 +3,28 @@ VALUES ('Neve', 5, 'Fiapo de manda', current_date, 'sujo', 'Daniel', 'medio');
 
 INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
 VALUES ('Sky', 5, 'Caramelo', current_date, 'jovem', 'Gomes', 'Grande');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte, data_adocao)
+VALUES ('Toto', 5, 'Pinsher', current_date, 'adulto', 'Daniel', 'pequeno', current_date);
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Miau', 2, 'Persa', current_date, 'magro e assustado', 'João', 'pequeno');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Luna', 1, 'Siamês', current_date, 'filhote resgatado de um incêndio', 'Carla', 'pequeno');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte, data_adocao)
+VALUES ('Tom', 3, 'Vira-lata', current_date, 'encontrado em uma árvore', 'Pedro', 'pequeno', current_date);
+
+-- Inserindo outros animais
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Rex', 4, 'Pastor Alemão', current_date, 'cansado e desidratado', 'Daniel', 'grande');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Nina', 5, 'Beagle', current_date, 'encontrada próximo a uma rodovia', 'Fernanda', 'médio');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte, data_adocao)
+VALUES ('Bolt', 2, 'Greyhound', current_date, 'resgatado de maus-tratos', 'Lucas', 'grande', current_date);
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Chico', 1, 'Coelho', current_date, 'pequeno e assustado', 'Mariana', 'pequeno');
